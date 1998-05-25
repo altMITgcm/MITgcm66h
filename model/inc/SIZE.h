7596f70e3bcd024561eb87a4ad7fe357eeafae52
Chris Hill <cnh@mit.edu>
1998-05-25 20:05:55 +0000

    Added extra IO features  - runtime on/off flags  - more reporting of configuration

--- model/inc/SIZE.h
+++ model/inc/SIZE.h
@@ -35,12 +35,12 @@ C     Nz  - No. points in Z for full process domain.
       INTEGER Ny
       INTEGER Nz
       PARAMETER (
-     &           sNx =  30, 
-     &           sNy =  30,
+     &           sNx =  60, 
+     &           sNy =  60,
      &           OLx =   3,
      &           OLy =   3,
-     &           nSx =   2,
-     &           nSy =   2,
+     &           nSx =   1,
+     &           nSy =   1,
      &           nPx =   1,
      &           nPy =   1,
      &           Nx  = sNx*nSx*nPx,
