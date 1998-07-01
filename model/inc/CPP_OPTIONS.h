47cb6a88ef792a7013fc52d18a1e8c75e153a73b
Alistair Adcroft <aadcroft@princeton.edu>
1998-07-01 19:49:37 +0000

    The time-averaging diagnostics has been modified to include averages of "intermediate" quantities that exist only on a tile. We currently only time-average K13, K23 and wVel but these should be usable as a template for other diagnostics. We have now split off these routines into a separate module called diags/. Use of these routines is enabled by the CPP flag ALLOW_DIAGNOSTICS set in model/inc/CPP_OPTIONS.h

--- model/inc/CPP_OPTIONS.h
+++ model/inc/CPP_OPTIONS.h
@@ -1,6 +1,10 @@
 C $Header$
 C
 
+C This enables the C-D grid method of integrating the Coriolis terms
 #define ALLOW_CD
+
+C This enables use of the diagnostics packages
+#define ALLOW_DIAGNOSTICS
 
 #include "CPP_EEOPTIONS.h"
