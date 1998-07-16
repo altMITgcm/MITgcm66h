6ad79fa418cbc008fa29af6159d5a4144221edc3
Alistair Adcroft <aadcroft@princeton.edu>
1998-07-16 15:23:43 +0000

    Added diagnostics for KapGM. Added GMmaxval to limit values of KapGM when using the Ri # formula.

--- model/inc/PARAMS.h
+++ model/inc/PARAMS.h
@@ -237,6 +237,7 @@ C     GMlength  - Length to use in Visbeck et al. formula for K (m)
 C     GMalpha   - alpha to use in Visbeck et al. formula for K
 C     GMdepth   - Depth over which to integrate Richardson # (Visbeck et al.)
 C     GMkbackground - background value of GM/Redi coefficient
+C     GMmaxval  - max. value of KapGM allowed in GM/Redi scheme
 C     startTime - Starting time for this integration ( s ).
 C     endTime   - Ending time for this integration ( s ).
 C     chkPtFreq  - Frequency of rolling check pointing ( s ).
@@ -265,7 +266,7 @@ C                           number times externForcingPeriod)
      & thetaMin, rSphere, rRSphere, f0, fCori, beta, viscAh, viscAz, viscA4, 
      & diffKhT, diffKzT, diffK4T, diffKhS, diffKzS, diffK4S, delT, 
      & tauCD, rCD, freeSurfFac,
-     & GMmaxslope,GMlength,GMalpha,GMdepth,GMkbackground,
+     & GMmaxslope,GMlength,GMalpha,GMdepth,GMkbackground,GMmaxval,
      & gravity, gBaro, rhonil, tRef, sRef,
      & endTime, chkPtFreq, pchkPtFreq, dumpFreq, taveFreq,
      & afFacMom, vfFacMom, pfFacMom, cfFacMom, foFacMom, mtFacMom,
@@ -306,6 +307,7 @@ C                           number times externForcingPeriod)
       _RL GMalpha
       _RL GMdepth
       _RL GMkbackground
+      _RL GMmaxval
       _RL gravity
       _RL gBaro
       _RL rhonil
