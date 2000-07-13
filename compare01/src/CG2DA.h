03525341fa3d0f6c315b677b0cf3c5a9e8c75bdd
cvs-fast-export <cvs-fast-export>
2000-07-13 14:22:58 +0000

    Synthetic commit for tag checkpoint30

--- compare01/src/CG2DA.h
+++
@@ -1,7 +0,0 @@
-C $Id$
-C     Common block used to hold 2d Laplace operator used in elliptic inversion 
-C     code.
-      COMMON /CG2DA/ aX, aY, aNorm2d
-      REAL aX(0:Nx+1,0:Ny+1)
-      REAL aY(0:Nx+1,0:Ny+1)
-      REAL aNorm2d
