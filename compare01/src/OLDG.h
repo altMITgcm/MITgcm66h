697a421b9a7be65e4482fa31e82131d281df0a41
cvs-fast-export <cvs-fast-export>
2000-11-30 20:02:41 +0000

    Synthetic commit for tag checkpoint33

--- compare01/src/OLDG.h
+++
@@ -1,8 +0,0 @@
-C $Id$
-C     Values of previous time derivatives used in Adams Bashforth
-C     timestepping.
-      COMMON /OldG/ guNM1, gvNM1, gtNM1, gsNM1
-      REAL guNM1 (_I3(nz,nx,ny))
-      REAL gvNM1 (_I3(nz,nx,ny))
-      REAL gtNM1 (_I3(nz,nx,ny))
-      REAL gsNM1 (_I3(nz,nx,ny))
