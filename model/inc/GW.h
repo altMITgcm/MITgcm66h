15209fe38d7cf8c6369e2bcea2ad5d74eec31c0d
Alistair Adcroft <aadcroft@princeton.edu>
2001-01-09 21:26:14 +0000

    Tentative implimentatin of OBCs.

--- model/inc/GW.h
+++ model/inc/GW.h
@@ -10,8 +10,7 @@ C     wVel  - zonal velocity (m/s, i=1 held at western face)
 C     gX, gXNM1 - Time tendencies at current and prvious time levels.
 
       COMMON /GW_R/ 
-     &                   wVel, gw,gwNm1
-      _RL  wVel (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+     &                   gw,gwNm1
       _RL  gW(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL  gWnm1(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
 
