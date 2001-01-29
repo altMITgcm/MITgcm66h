c76cfcbdcc8a4c707e2416ee87bddd1c39fa74f8
Chris Hill <cnh@mit.edu>
2001-01-29 01:18:06 +0000

    o Definitions of meaning and units for bottomDragLinear (1/s),   bottomDragQuadratic (1/m) added to PARAMS.h and 5lAIM_ec   experiment updated.
    o New statically defined yG array used in zonal_filt_init

--- model/inc/PARAMS.h
+++ model/inc/PARAMS.h
@@ -359,6 +359,8 @@ C     latFFTFiltLo       - Low latitude for FFT filtering of latitude
 C                          circles ( see filter*.F )
 C     Ro_SeaLevel        - standard position of Sea-Level in "R" coordinate, used as
 C                          starting value (k=1) for vertical coordinate (rf(1)=Ro_SeaLevel)
+C     bottomDragLinear   - Drag coefficient built in to core dynamics
+C         "     Quadratic  ( linear: 1/s, quadratic: 1/m )
       COMMON /PARM_R/ cg2dTargetResidual, cg2dpcOffDFac, 
      & cg3dTargetResidual,
      & delP, delZ, delR, delX, delY, 
