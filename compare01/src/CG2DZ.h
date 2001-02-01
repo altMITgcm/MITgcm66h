c319d73f31b4ece8eeb9d89858149b664f269c75
cvs-fast-export <cvs-fast-export>
2001-02-01 19:32:52 +0000

    Synthetic commit for tag checkpoint34

--- compare01/src/CG2DZ.h
+++
@@ -1,7 +0,0 @@
-C $Id$
-C     Common block used to hold precondidioner for 2d Laplace operator inversion
-C     code.
-      COMMON /CG2DZ/ pC, pX, pY
-      REAL pC(Nx,Ny)
-      REAL pX(0:Nx+1,0:Ny+1)
-      REAL pY(0:Nx+1,0:Ny+1)
