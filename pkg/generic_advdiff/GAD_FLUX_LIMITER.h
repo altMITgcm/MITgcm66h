64d1cb84097f51695d8f737315ff0079b0df9703
Alistair Adcroft <aadcroft@princeton.edu>
2001-05-30 19:34:48 +0000

    Added "gad" package. Needs generalizing to allow selection of advection schemes at run-time and different schemes for each tracer.

---
+++ pkg/generic_advdiff/GAD_FLUX_LIMITER.h
@@ -0,0 +1,12 @@
+C $Header$
+C $Name$
+
+C Statement function to describe flux limiter
+      _RL Limiter
+C Upwind        Limiter(Cr)=0.
+C Lax-Wendroff  Limiter(Cr)=1.
+C Suberbee      Limiter(Cr)=max(0.,max(min(1.,2*Cr),min(2.,Cr)))
+
+c     Limiter(Cr)=0.
+c     Limiter(Cr)=1.
+      Limiter(Cr)=max(0.,max(min(1.,2*Cr),min(2.,Cr)))
