1bbadb0706844856f14f0707074e618254cc63fa
Jean-Michel Campin <jmc@ocean.mit.edu>
2001-06-15 15:14:56 +0000

    extend S2 Shapiro Filter : new parameters nShapTrPhys and nShapUVPhys allow to switch from numerical-space filter (default)             to physical-space filter  (as S2g)             or a combination of both.

--- pkg/shap_filt/SHAP_FILT.h
+++ pkg/shap_filt/SHAP_FILT.h
@@ -3,19 +3,25 @@ C $Name$
 
 #ifdef ALLOW_SHAP_FILT
 
-C     Package flag 
+C-    Package flag 
 C     Shap_funct : define which Shapiro Filter function is used
 C        = 1  (S1) : [1 - d_xx^n - d_yy^n]
 C        = 4  (S4) : [1 - d_xx^n][1- d_yy^n]
 C        = 2  (S2) : [1 - (d_xx+d_yy)^n]
-      INTEGER nShapT, nShapUV, Shap_funct
-      COMMON /SHAP_FILT_INT/ nShapT, nShapUV, Shap_funct
+C     nShap_Tr,UV : (total) power of the filter for Tracer, Velocity
+C  available only with Shap_funct=2 : 
+C     combine filter in Physical space (power of nShap__Phys) 
+C        and pure numerical filter (power nShap - nShap_Phys) 
 
-C     Shapiro Filter parameters
-      _RL Shap_Trtau, Shap_TrDamp, Shap_TrLength
-      _RL Shap_uvtau, Shap_uvDamp, Shap_uvLength
+      INTEGER  Shap_funct, nShapT, nShapUV, nShapTrPhys, nShapUVPhys
+      COMMON /SHAP_FILT_INT/ Shap_funct, nShapT , nShapTrPhys, 
+     &                                   nShapUV, nShapUVPhys
+
+C-    Shapiro Filter parameters
+      _RL Shap_Trtau, Shap_TrLength
+      _RL Shap_uvtau, Shap_uvLength
       COMMON /SHAP_FILT_PARAMS/ 
-     &                  Shap_Trtau, Shap_TrDamp, Shap_TrLength,
-     &                  Shap_uvtau, Shap_uvDamp, Shap_uvLength
+     &                  Shap_Trtau, Shap_TrLength,
+     &                  Shap_uvtau, Shap_uvLength
 
 #endif /* ALLOW_SHAP_FILT */
