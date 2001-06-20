ac9f1f739eb619c465945bc41ca98f6c37293fa8
jamous <jamous>
2001-06-20 16:15:07 +0000

    Wrote real constants in double precision in min, max arguments (necessary when compiling on IBM with mpxlf95)

--- pkg/generic_advdiff/GAD_FLUX_LIMITER.h
+++ pkg/generic_advdiff/GAD_FLUX_LIMITER.h
@@ -9,4 +9,4 @@ C Suberbee      Limiter(Cr)=max(0.,max(min(1.,2*Cr),min(2.,Cr)))
 
 c     Limiter(Cr)=0.
 c     Limiter(Cr)=1.
-      Limiter(Cr)=max(0.,max(min(1.,2*Cr),min(2.,Cr)))
+      Limiter(Cr)=max(0.d0,max(min(1.d0,2.d0*Cr),min(2.d0,Cr)))
