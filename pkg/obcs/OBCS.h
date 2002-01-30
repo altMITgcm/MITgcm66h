d797d8246fc7331264493fc88a80c13c3c3ba234
Jean-Michel Campin <jmc@ocean.mit.edu>
2002-01-30 04:22:31 +0000

    NonLin_FreeSurf implemented with OBC (but not yet radiative OBC).

--- pkg/obcs/OBCS.h
+++ pkg/obcs/OBCS.h
@@ -98,4 +98,18 @@ C
       _RS OBWw (1-Oly:sNy+Oly,Nr,nSx,nSy)
 #endif /* ALLOW_NONHYDROSTATIC */
 
+#ifdef NONLIN_FRSURF
+      COMMON /GRID_OB_NLFS/ 
+     &  OBNhfac0,OBShfac0,OBEhfac0,OBWhfac0,
+     &  OBNeta,  OBSeta,  OBEeta,  OBWeta
+      _RS OBNhfac0(1-Olx:sNx+Olx,nSx,nSy)
+      _RS OBShfac0(1-Olx:sNx+Olx,nSx,nSy)
+      _RS OBEhfac0(1-Oly:sNy+Oly,nSx,nSy)
+      _RS OBWhfac0(1-Oly:sNy+Oly,nSx,nSy)
+      _RS OBNeta (1-Olx:sNx+Olx,nSx,nSy)
+      _RS OBSeta (1-Olx:sNx+Olx,nSx,nSy)
+      _RS OBEeta (1-Oly:sNy+Oly,nSx,nSy)
+      _RS OBWeta (1-Oly:sNy+Oly,nSx,nSy)
+#endif /* NONLIN_FRSURF */
+
 #endif /* ALLOW_OBCS */
