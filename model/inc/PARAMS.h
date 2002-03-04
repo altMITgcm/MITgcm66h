23d46552189cca6ceff6c19c004c5037ecb11390
Alistair Adcroft <aadcroft@princeton.edu>
2002-03-04 17:26:41 +0000

    Added PTRACERS package
    This allows an arbitrary number of passive tracers to be integrated
    forward simultaneously with the dynamicaly model.
     + Implemented so far:
        - basic forward algorithm (time-stepping, advection, diffusion, convection)
        - I/O and checkpointing
        - GM/Redi  *but*  using the GM/Redi coefficient of Salt
     + Not implemented so far:
        - KPP
        - OBCS
     + No specific example supplied (yet) but global_ocean.90x40x15 has the
       necessary data.ptracer file. Simply use -enable=ptracers and uncomment
       line in data.pkg. PTRACER01 then reproduces Salt exactly.
     + This package is disabled by default since it increases storage.

--- model/inc/PARAMS.h
+++ model/inc/PARAMS.h
@@ -577,7 +577,9 @@ C Logical flags for selecting packages
       LOGICAL useSHAP_FILT
       LOGICAL useZONAL_FILT
       LOGICAL useFLT
+      LOGICAL usePTRACERS
       COMMON /PARM_PACKAGES/
      &        useKPP, useGMRedi, useOBCS, useAIM, useECCO, 
-     &        useSHAP_FILT, useZONAL_FILT, useGrdchk, useFLT
+     &        useSHAP_FILT, useZONAL_FILT, useGrdchk, useFLT,
+     &        usePTRACERS
 
