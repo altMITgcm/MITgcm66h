6152487cb053d6be823c0f3b9f315222c8370b13
Jean-Michel Campin <jmc@ocean.mit.edu>
2002-11-22 02:50:34 +0000

    keep the oragraphic field in a common block here  (moved from a local common of ini_depth)

--- model/inc/SURFACE.h
+++ model/inc/SURFACE.h
@@ -17,12 +17,14 @@ C     *==========================================================*
 C     \ev
 CEOP
 
-C--   COMMON /SOLVE_BAROT/  Barotropic variables common block
-C     Bo_surf  :: Boyancy|1/rho [ocean|atmos] at surface level [= g | alpha(p_o)]
-C     recip_Bo     ::  1/Bo_surf
-      COMMON /SOLVE_BAROT/ Bo_surf, recip_Bo
-      _RL  Bo_surf(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+C--   COMMON /SURF_FIXED/  fixed surface arrays (Real)
+C     Bo_surf  :: Boyancy|1/rho [ocean|atmos] at surface level [=g|alpha(p_o)]
+C     recip_Bo :: 1/Bo_surf
+C     topoZ    :: topographic height [m] (used mainly for atmosphere)
+      COMMON /SURF_FIXED/ Bo_surf, recip_Bo, topoZ
+      _RL  Bo_surf (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL  recip_Bo(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RS  topoZ   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 
 C--   COMMON /SURF_INDEX/ Common block for surface related index
 C     ksurfC ::  vertical index of the surface tracer cell
