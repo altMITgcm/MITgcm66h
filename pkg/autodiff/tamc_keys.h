12f4bf7f019ecbf9ecc26294b8a5c0c595971532
cvs-fast-export <cvs-fast-export>
2003-03-07 02:45:48 +0000

    Synthetic commit for tag c49_ctrl

--- pkg/autodiff/tamc_keys.h
+++
@@ -1,8 +0,0 @@
-C $Header$
-C
-C     /==========================================================\
-C     | tamc_keys.h keys required by TAMC for record computation |
-C     |==========================================================|
-C     \==========================================================/
-      integer           key, ikey
-      common /tamckeys/ key, ikey
