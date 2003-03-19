95abbbe35d8dc0145f1c25d235c7679d716d9e43
Patrick Heimbach <heimbach@mit.edu>
2003-03-19 20:18:21 +0000

    Adapting some verification setup headers to latest c50.

--- verification/natl_box_adjoint/code_seaice/GMREDI_OPTIONS.h
+++ verification/natl_box_adjoint/code_seaice/GMREDI_OPTIONS.h
@@ -11,16 +11,18 @@ C GM/Redi is enabled with ALLOW_GMREDI in CPP_OPTIONS.h
 
 #ifdef ALLOW_GMREDI
 
-C Specify as tapering scheme either 'orig' or 'clipping',
-C otherwise one of the following is used:
-C 'linear', 'gkw91', 'dm95', 'ldd97'
-#undef GM_TAPER_ORIG_CLIPPING
-#undef GM_TAPER_AC02
-#define GM_TAPER_REST
+C Designed to simplify the Ajoint code:
+C  exclude the clipping/tapering part of the code that is not used
+#define GM_EXCLUDE_CLIPPING
+#define GM_EXCLUDE_AC02_TAP
+#undef  GM_EXCLUDE_TAPERING 
+ 
+C This allows to use Visbeck et al formulation to compute K_GM+Redi
+#undef  GM_VISBECK_VARIABLE_K
 
 C This allows the leading diagonal (top two rows) to be non-unity
 C (a feature required when tapering adiabatically).
-#define GM_NON_UNITY_DIAGONAL
+#define  GM_NON_UNITY_DIAGONAL
 
 C Allows to use different values of K_GM and K_Redi ; also to
 C be used with the advective form (Bolus velocity) of GM
@@ -30,8 +32,9 @@ C Allows to use the advective form (Bolus velocity) of GM
 C  instead of the Skew-Flux form (=default)
 #undef  GM_BOLUS_ADVEC
 
-C This allows to use Visbeck et al formulation to compute K_GM+Redi
-#undef  GM_VISBECK_VARIABLE_K
-
+C Following option avoids specific recomputation in adjoint
+C routines of gmredi_x/y/rtransport
+C It's not needed, only for tests, and very memory-consuming
+#undef GM_AUTODIFF_EXCESSIVE_STORE
 
 #endif /* ALLOW_GMREDI */
