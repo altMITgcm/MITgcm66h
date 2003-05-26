564c0164394dec85c0e652597d6e4b73af3ecfb7
Dimitris Menemenlis <Dimitris.Menemenlis@jpl.nasa.gov>
2003-05-26 07:49:18 +0000

    o Patrick's adjoint-model storage directives for pkg/seaice

--- verification/natl_box_adjoint/code_seaice/tamc.h
+++ verification/natl_box_adjoint/code_seaice/tamc.h
@@ -113,7 +113,7 @@ c     and writing data.
       INTEGER act0, act1, act2, act3, act4
       INTEGER max0, max1, max2, max3
       INTEGER iikey, kkey, passkey, igadkey, 
-     &        itdkey, idynkey, igmkey, ikppkey
+     &        itdkey, idynkey, igmkey, ikppkey, iicekey
 
 c     ================================================================
 c     END OF HEADER TAMC
