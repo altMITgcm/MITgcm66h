4de45b65abd40a58c7843dac1cf5020b238d27ca
Patrick Heimbach <heimbach@mit.edu>
2003-05-27 22:43:44 +0000

    o Modifications for adjoint of SEAICE_ALLOW_DYNAMICS   - extended state in checkpoint_lev...   - in lsr.F, replaced GO TO by fixed loop index (also avoid DO WHILE)   - YEAH: in lsr.F, (first) successful use of CADJ LOOP = ITERATION   - avoid stoing in S/R budget for now   - in dynsolver.F, avoid NPSEUDO loop for adjoint   - new comlev1_lsr in the_main_loop

--- pkg/autodiff/checkpoint_lev3_directives.h
+++ pkg/autodiff/checkpoint_lev3_directives.h
@@ -227,4 +227,8 @@ CADJ STORE heff     = tapelev3, key = ilev_3
 CADJ STORE hsnow    = tapelev3, key = ilev_3
 CADJ STORE tice     = tapelev3, key = ilev_3
 CADJ STORE runoff   = tapelev3, key = ilev_3
+# ifdef SEAICE_ALLOW_DYNAMICS
+CADJ STORE uice     = tapelev3, key = ilev_3
+CADJ STORE vice     = tapelev3, key = ilev_3
+# endif
 #endif /* ALLOW_SEAICE */
