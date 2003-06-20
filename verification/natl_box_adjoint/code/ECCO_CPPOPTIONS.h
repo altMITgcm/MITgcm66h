d47ec2746deb47b93bb15d2ac2daf52cac8dd224
Patrick Heimbach <heimbach@mit.edu>
2003-06-20 05:32:45 +0000

    Modif's to enable tangent linear run in ECCO environment o main problem is missing call mdsreadfield out of exf_set_gen o incorrect g_active_write_... o (re-) init of bar files and g_bar files

--- verification/natl_box_adjoint/code/ECCO_CPPOPTIONS.h
+++ verification/natl_box_adjoint/code/ECCO_CPPOPTIONS.h
@@ -13,6 +13,7 @@ C o include dump of snap shots for checks
 #undef  ALLOW_ECCO_FORWARD_RUN
 #undef  ALLOW_ECCO_DIAGNOSTICS_RUN
 #undef  ALLOW_ADJOINT_RUN
+#undef  ALLOW_TANGENTLINEAR_RUN
 #define  ALLOW_GRADIENT_CHECK
 #define  ALLOW_ECCO_OPTIMIZATION
 
@@ -100,6 +101,7 @@ C       >>> Cost function contributions
 # undef ALLOW_CTDT_COST_CONTRIBUTION
 # undef ALLOW_CTDS_COST_CONTRIBUTION
 # undef ALLOW_XBT_COST_CONTRIBUTION
+
 # undef ALLOW_COST_ATLANTIC
 
 c       >>> Open boundaries
