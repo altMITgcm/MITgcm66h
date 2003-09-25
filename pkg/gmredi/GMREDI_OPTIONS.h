12d9630d789b07c0ca6af3f6a9b9c5c708ac462c
Patrick Heimbach <heimbach@mit.edu>
2003-09-25 22:14:09 +0000

    o updating default GMREDI_OPTIONS.h

--- pkg/gmredi/GMREDI_OPTIONS.h
+++ pkg/gmredi/GMREDI_OPTIONS.h
@@ -11,14 +11,14 @@ C GM/Redi is enabled with ALLOW_GMREDI in CPP_OPTIONS.h
 
 #ifdef ALLOW_GMREDI
 
-C Design to simplify the Ajoint code: 
+C Designed to simplify the Ajoint code:
 C  exclude the clipping/tapering part of the code that is not used
-C #define GM_EXCLUDE_CLIPPING
-C #define GM_EXCLUDE_AC02_TAP
-C #define GM_EXCLUDE_TAPERING
+#define GM_EXCLUDE_CLIPPING
+#define GM_EXCLUDE_AC02_TAP
+#undef  GM_EXCLUDE_TAPERING 
  
 C This allows to use Visbeck et al formulation to compute K_GM+Redi
-#undef GM_VISBECK_VARIABLE_K
+#undef  GM_VISBECK_VARIABLE_K
 
 C This allows the leading diagonal (top two rows) to be non-unity
 C (a feature required when tapering adiabatically).
@@ -32,5 +32,9 @@ C Allows to use the advective form (Bolus velocity) of GM
 C  instead of the Skew-Flux form (=default)
 #undef  GM_BOLUS_ADVEC
 
+C Following option avoids specific recomputation in adjoint
+C routines of gmredi_x/y/rtransport
+C It's not needed, only for tests, and very memory-consuming
+#undef GM_AUTODIFF_EXCESSIVE_STORE
 
 #endif /* ALLOW_GMREDI */
