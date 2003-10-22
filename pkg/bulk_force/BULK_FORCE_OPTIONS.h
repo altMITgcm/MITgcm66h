add8d7451e9052e1a73980b718e824829d72148b
Jean-Michel Campin <jmc@ocean.mit.edu>
2003-10-22 18:38:48 +0000

    add "#undef CONSERV_BULKF" to clarify what is really used.

--- pkg/bulk_force/BULK_FORCE_OPTIONS.h
+++ pkg/bulk_force/BULK_FORCE_OPTIONS.h
@@ -4,11 +4,13 @@ C $Name$
 #ifndef BULK_FORCE_OPTIONS_H
 #define BULK_FORCE_OPTIONS_H
 #include "PACKAGES_CONFIG.h"
-#ifdef ALLOW_BULK_FORCE
 
 #include "CPP_OPTIONS.h"
 
+#ifdef ALLOW_BULK_FORCE
 C CPP Macros go here
+
+#undef CONSERV_BULKF
 
 #endif /* ALLOW_BULK_FORCE */
 #endif /* BULK_FORCE_OPTIONS_H */
