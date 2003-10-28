648a8be343480cccaab1cbe10bc48f0633cff261
Ed Hill <ed@eh3.com>
2003-10-28 20:03:25 +0000

    o cleanup for PACKAGES_CONFIG.h  o diff with the default "pkg/gmredi/GMREDI_OPTIONS.h" is much cleaner

--- verification/ideal_2D_oce/code/GMREDI_OPTIONS.h
+++ verification/ideal_2D_oce/code/GMREDI_OPTIONS.h
@@ -4,8 +4,6 @@ C $Name$
 C CPP options file for GM/Redi package
 C
 C Use this file for selecting options within the GM/Redi package
-C
-C GM/Redi is enabled with ALLOW_GMREDI in CPP_OPTIONS.h
 
 #ifndef GMREDI_OPTIONS_H
 #define GMREDI_OPTIONS_H
@@ -19,6 +17,9 @@ C  exclude the clipping/tapering part of the code that is not used
 C #define GM_EXCLUDE_CLIPPING
 C #define GM_EXCLUDE_AC02_TAP
 C #define GM_EXCLUDE_TAPERING
+ 
+C This allows to use Visbeck et al formulation to compute K_GM+Redi
+#define GM_VISBECK_VARIABLE_K
 
 C This allows the leading diagonal (top two rows) to be non-unity
 C (a feature required when tapering adiabatically).
@@ -31,9 +32,6 @@ C be used with the advective form (Bolus velocity) of GM
 C Allows to use the advective form (Bolus velocity) of GM
 C  instead of the Skew-Flux form (=default)
 #define GM_BOLUS_ADVEC
-
-C This allows to use Visbeck et al formulation to compute K_GM+Redi
-#define GM_VISBECK_VARIABLE_K
 
 
 #endif /* ALLOW_GMREDI */
