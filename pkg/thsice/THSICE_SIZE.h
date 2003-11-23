37f980104e566c906d9cb050b8379ceb9aa383e1
Jean-Michel Campin <jmc@ocean.mit.edu>
2003-11-23 01:20:13 +0000

    new pkg "thSIce" (replace therm_seaice).

---
+++ pkg/thsice/THSICE_SIZE.h
@@ -0,0 +1,15 @@
+C $Header$
+C $Name$
+
+#ifdef ALLOW_THSICE
+C     *==========================================================*
+C     | THSICE_SIZE.h Declare size of arrays for Therm_SeaIce pkg
+C     *==========================================================*
+
+C.. number layers of ice 
+C     nlyr   ::   maximum number of ice layers
+      INTEGER nlyr
+      PARAMETER (nlyr = 2)
+
+#endif /* ALLOW_THSICE */
+
