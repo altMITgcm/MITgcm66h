20f9fc1ab31ae63d231c04df3e6e1c2f3178f80e
Patrick Heimbach <heimbach@mit.edu>
2003-11-25 16:07:03 +0000

    Fixing OPTIONS files for use of exf: o ALLOW_ECCO not needed o ECCO_CPPOPTIONS.h should not be included from   EXF_OPTIONS.h/CAL_OPTIONS.h, but from CPP_OPTIONS.h   as has been the case in the past o All cal/exf related options are currently set in   single header file. Should remain so until we   agree on a consistent new policy.

--- pkg/exf/EXF_OPTIONS.h
+++ pkg/exf/EXF_OPTIONS.h
@@ -10,8 +10,12 @@ C $Name$
 
 C Despite the comments below, this package is currently configured by
 C ECCO_CPPOPTIONS.h and so you should not customize this file.
-#define ALLOW_ECCO
-#include "ECCO_CPPOPTIONS.h"
+
+CPH >>>>>> THIS SHOULD BE INCLUDED BY CPP_OPTIONS.h <<<<<<
+CPH >>>>>> TO AVOID ORDERING OF HEADERS TO MATTER   <<<<<<
+CPH >>>>>> UNTIL WE DECIDE ON CONSISTENT CHANGE IN  <<<<<<
+CPH >>>>>> POLICY                                   <<<<<<
+CPH#include "ECCO_CPPOPTIONS.h"
 
 C CPP flags controlling which code is included in the files that
 C will be compiled.
