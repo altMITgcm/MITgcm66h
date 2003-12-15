c9404e598d4ea5c12fd392267200952d11ae474d
Jean-Michel Campin <jmc@ocean.mit.edu>
2003-12-15 01:54:33 +0000

    add logical flag useCoupler for a multi-components set-up.

--- eesupp/inc/EEPARAMS.h
+++ eesupp/inc/EEPARAMS.h
@@ -120,12 +120,14 @@ C     eeBootError - Flag indicating error during multi-processing
 C     eeEndError    initialisation/termination.
 C     fatalError  - Flag used to indicate that the model is ended with
 C                   an error
+C     useCoupler  - use Coupler for a multi-components set-up
       COMMON /EEPARAMS_L/ eeBootError, fatalError, eeEndError,
-     &  useCubedSphereExchange
+     &  useCubedSphereExchange, useCoupler
       LOGICAL eeBootError
       LOGICAL eeEndError
       LOGICAL fatalError
       LOGICAL useCubedSphereExchange
+      LOGICAL useCoupler
 
 C--   COMMON /EPARAMS_I/ Execution environment public integer variables.
 C     errorMessageUnit    - Fortran IO unit for error messages
