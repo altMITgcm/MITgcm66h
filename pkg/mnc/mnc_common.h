8647ade87feb6052416836d97671f418af95c506
Ed Hill <ed@eh3.com>
2004-03-09 03:57:25 +0000

    o minor mnc changes

--- pkg/mnc/mnc_common.h
+++ pkg/mnc/mnc_common.h
@@ -98,7 +98,7 @@ C---+----1----+----2----+----3----+----4----+----5----+----6----+----7-|--+----|
       parameter ( MNC_MAX_ID   = 1000 )
       parameter ( MNC_MAX_CHAR =  100 )
       parameter ( MNC_MAX_INFO =  200 )
-      parameter ( MNC_CW_MAX_I =  100 )
+      parameter ( MNC_CW_MAX_I =  200 )
 
       COMMON /MNC_VARS/
      &     mnc_blank_name, 
