af3b753499f3ba957fbacfce69f5db2b712eeffd
Jean-Michel Campin <jmc@ocean.mit.edu>
2004-05-21 19:42:26 +0000

    new version of ocean-coupler interface for AIM+thSIce and ocean models

--- pkg/ocn_compon_interf/CPL_PARAMS.h
+++ pkg/ocn_compon_interf/CPL_PARAMS.h
@@ -10,14 +10,20 @@ C     |   this version is specific to 1 component (ocean)
 C     *==========================================================*
 
 C--   COMMON /CPL_OCN_PAR_L/: logical parameters
+C     cpl_earlyExpImpCall :: call coupler early in the time stepping call sequence
 C     useImportHFlx :: True => use the Imported HeatFlux from couler
 C     useImportFW   :: True => use the Imported Fresh Water flux fr cpl
 C     useImportTau  :: True => use the Imported Wind-Stress from couler
-      COMMON /LAND_PAR_L/
-     &  useImportHFlx, useImportFW, useImportTau
+C     useImportSLP  :: True => use the Imported Sea-level Pressure as
+C                              atmospheric loading
+      COMMON /CPL_OCN_PAR_L/
+     &  cpl_earlyExpImpCall,
+     &  useImportHFlx, useImportFW, useImportTau, useImportSLP
+      LOGICAL cpl_earlyExpImpCall
       LOGICAL useImportHFlx
       LOGICAL useImportFW
       LOGICAL useImportTau
+      LOGICAL useImportSLP
 
 C--   COMMON /CPL_OCN_PAR_C/: Character valued parameters
 c     CHARACTER*(MAX_LEN_FNAM) cpl_ocnFile
