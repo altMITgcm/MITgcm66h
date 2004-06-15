38d122435114fffbe6e86fabc42d901646bd3a77
Andrea Molod <molod@ocean.mit.edu>
2004-06-15 17:19:55 +0000

    Ensure gridalt stuff meant for fizhi is only used with fizhi

--- pkg/gridalt/gridalt_mapping.h
+++ pkg/gridalt/gridalt_mapping.h
@@ -3,9 +3,11 @@ C $Name$
 
 c Alternate grid Mapping Common
 c ------------------------------
+#ifdef ALLOW_FIZHI
       common /gridalt_mapping/ nlperdyn,dpphys0,dpphys,
      .                                             dxfalt,dyfalt,drfalt
       integer nlperdyn(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,Nsy)
       _RL dpphys0(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,nSy)
       _RL dpphys(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,nSy)
       _RL dxfalt,dyfalt,drfalt
+#endif
