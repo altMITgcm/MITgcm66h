1ec48be8ad3dfea061f4decc2f73bdd2a0307fc7
Andrea Molod <molod@ocean.mit.edu>
2004-07-13 21:21:05 +0000

    Debugging fizhi

--- pkg/gridalt/gridalt_mapping.h
+++ pkg/gridalt/gridalt_mapping.h
@@ -1,13 +1,15 @@
 C $Header$
 C $Name$
 
+#include "PACKAGES_CONFIG.h"
+
 c Alternate grid Mapping Common
 c ------------------------------
 #ifdef ALLOW_FIZHI
-      common /gridalt_mapping/ nlperdyn,dpphys0,dpphys,
-     .                                             dxfalt,dyfalt,drfalt
       integer nlperdyn(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,Nsy)
       _RL dpphys0(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,nSy)
       _RL dpphys(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,nSy)
       _RL dxfalt,dyfalt,drfalt
+      common /gridalt_mapping/ nlperdyn,dpphys0,dpphys,
+     .                                             dxfalt,dyfalt,drfalt
 #endif
