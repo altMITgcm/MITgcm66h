8ee88a478241e1e98c00bd4dca43ba059d0715f0
Ed Hill <ed@eh3.com>
2004-09-07 16:19:30 +0000

    o add Antti Westerlund's extensions to make flt work with 3D velocity    fields

--- pkg/flt/FLT_CPPOPTIONS.h
+++ pkg/flt/FLT_CPPOPTIONS.h
@@ -4,5 +4,17 @@ C $Name$
 #include "CPP_OPTIONS.h"
 
 c Include/Exclude part that allows 3-dimensional advection of floats
-c (not tested!)
-#undef ALLOW_3D_FLT
+c 
+#define ALLOW_3D_FLT
+
+c Use the alternative method of adding random noise to float advection
+c 
+#define USE_FLT_ALT_NOISE
+
+c Add noise also to the vertical velocity of 3D floats
+c  
+#ifdef ALLOW_3D_FLT
+#define ALLOW_FLT_3D_NOISE
+#endif
+
+
