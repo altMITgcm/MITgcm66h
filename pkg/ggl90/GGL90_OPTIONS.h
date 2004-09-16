49be01dc5d49420d8cf85a08f515202c4e8f777a
Martin Losch <Martin.Losch@awi.de>
2004-09-16 11:27:18 +0000

    o initial check-in of TKE-model of Gaspar et al. 1990

---
+++ pkg/ggl90/GGL90_OPTIONS.h
@@ -0,0 +1,21 @@
+C$Header$
+C$Name$
+C     /=============================================================\
+C     | GGL90_OPTIONS.h                                             |
+C     | o CPP options file for GGL90 package.                       |
+C     |=============================================================|
+C     | Use this file for selecting options within the GGL90        |
+C     | package. GGL90 is enabled with ALLOW_GGL90 in CPP_OPTIONS.h |
+C     \=============================================================/
+
+#ifndef GGL90_OPTIONS_H
+#define GGL90_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+
+#ifdef ALLOW_GGL90
+
+#include "CPP_OPTIONS.h"
+
+#endif /* ALLOW_GGL90 */
+#endif /* GGL90_OPTIONS_H */
+
