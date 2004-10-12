59636d2999c0ce5985cd1eb943120e07fd928f02
Patrick Heimbach <heimbach@mit.edu>
2004-10-12 15:14:16 +0000

    updating global1x1_tot

--- verification/global1x1_tot/code/GMREDI_OPTIONS.h
+++ verification/global1x1_tot/code/GMREDI_OPTIONS.h
@@ -24,7 +24,7 @@ C This allows to use Visbeck et al formulation to compute K_GM+Redi
 
 C This allows the leading diagonal (top two rows) to be non-unity
 C (a feature required when tapering adiabatically).
-#undef  GM_NON_UNITY_DIAGONAL
+#define  GM_NON_UNITY_DIAGONAL
 
 C Allows to use different values of K_GM and K_Redi ; also to
 C be used with the advective form (Bolus velocity) of GM
