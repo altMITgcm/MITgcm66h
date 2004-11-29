2376038f4c7dd86b3180f9ba126f948ceeeb2642
Martin Losch <Martin.Losch@awi.de>
2004-11-29 22:38:21 +0000

    add another verification experiment that tests both the cfc-pkg and the gchem-pkg with GCHEM_SEPARATE_FORCING undefined, that is with reactive tendencies treated inline with regular timestepping

---
+++ verification/cfc_example/code/GCHEM_OPTIONS.h
@@ -0,0 +1,19 @@
+C$Header$
+C$Name$
+
+#include "CPP_OPTIONS.h"
+#ifdef ALLOW_PTRACERS
+
+C    !ROUTINE: GCHEM_OPTIONS.h
+C    !INTERFACE:
+
+C    !DESCRIPTION:
+c options for biogeochemistry package
+#undef GCHEM_SEPARATE_FORCING
+#define ALLOW_CFC
+#undef DIC_BIOTIC
+#undef  ALLOW_FE
+
+#undef ALLOW_DIC_COST
+
+#endif
