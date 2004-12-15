fd764bcce631810c9a2b1e35eeb361f2c8336f23
Dimitris Menemenlis <Dimitris.Menemenlis@jpl.nasa.gov>
2004-12-15 08:29:56 +0000

    increased MAX_NO_PROCS to 2048

--- eesupp/inc/EEPARAMS.h
+++ eesupp/inc/EEPARAMS.h
@@ -111,7 +111,7 @@ C     MAX_NO_BARRIERS - Maximum number of distinct thread "barriers"
       INTEGER MAX_NO_THREADS
       PARAMETER ( MAX_NO_THREADS =   32 )
       INTEGER MAX_NO_PROCS
-      PARAMETER ( MAX_NO_PROCS   =  1024 )
+      PARAMETER ( MAX_NO_PROCS   =  2048 )
       INTEGER MAX_NO_BARRIERS
       PARAMETER ( MAX_NO_BARRIERS = 1 )
 
