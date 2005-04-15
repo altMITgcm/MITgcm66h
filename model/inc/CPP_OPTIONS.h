5aac11642fbb53917a125831f53ffc4ec3bf4ba5
Jean-Michel Campin <jmc@ocean.mit.edu>
2005-04-15 13:19:46 +0000

    #undef ALLOW_ADAMSBASHFORTH_3 : disable ADAMS-BASHFORTH-3 code by default

--- model/inc/CPP_OPTIONS.h
+++ model/inc/CPP_OPTIONS.h
@@ -22,6 +22,9 @@ C o Include/exclude call to S/R CALC_DIFFUSIVITY
 C o Include/exclude Implicit vertical advection code
 #define INCLUDE_IMPLVERTADV_CODE
 
+C o Include/exclude AdamsBashforth-3rd-Order code
+#undef ALLOW_ADAMSBASHFORTH_3
+
 C o Include/exclude nonHydrostatic code
 #undef ALLOW_NONHYDROSTATIC
 
