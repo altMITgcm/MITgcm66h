0dc4cf23a0c81ada2b818f75b1e25ec7057c5dfb
Alistair Adcroft <aadcroft@princeton.edu>
2005-05-31 14:49:38 +0000

    Added run-time parameters nh_Am2 which scales the non-hydrostatic terms and changes internal scales (i.e. allows convection at different Rayleigh numbers).

--- model/inc/PARAMS.h
+++ model/inc/PARAMS.h
@@ -600,9 +600,10 @@ C      --"-"--  Quadratic  ( linear: 1/s, quadratic: 1/m )
      & hFacMinDr, hFacMinDp,
      & horiVertRatio, recip_horiVertRatio,
      & ivdc_kappa, Ro_SeaLevel,
-     & bottomDragLinear,bottomDragQuadratic,
+     & bottomDragLinear,bottomDragQuadratic,nh_Am2,
      & tCylIn, tCylOut
 
+      _RL nh_Am2
       _RL cg2dTargetResidual
       _RL cg2dTargetResWunit
       _RL cg3dTargetResidual
