2eaa1fc74af015130dfbd21c7c5e22655c23812d
Ed Hill <ed@eh3.com>
2005-08-11 03:22:18 +0000

    o increase the default buffer size

--- pkg/mnc/MNC_SIZE.h
+++ pkg/mnc/MNC_SIZE.h
@@ -8,7 +8,7 @@ C     writes are checked and MNC will terminate with a sensible error
 C     message if the buffer is not large enough for all reads/writes.
 
       INTEGER     MNC_MAX_BUFF
-      PARAMETER ( MNC_MAX_BUFF = 5000 + sNx + 2*OLx + sNy + 2*OLy )
+      PARAMETER ( MNC_MAX_BUFF = 15000 + sNx + 2*OLx + sNy + 2*OLy )
 
 CEH3 ;;; Local Variables: ***
 CEH3 ;;; mode:fortran ***
