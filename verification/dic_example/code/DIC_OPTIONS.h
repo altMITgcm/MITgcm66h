3271ee7c52c5a607c7f63a4a2bb7362fa061a766
Stephanie Dutkiewicz <stephd@ocean.mit.edu>
2005-08-18 19:31:30 +0000

    o add file which has options for DIC code (which have been moved   from GCHEM_OPTIONS.h): better syntax

---
+++ verification/dic_example/code/DIC_OPTIONS.h
@@ -0,0 +1,21 @@
+C $Header$
+C $Name$
+
+#ifndef DIC_OPTIONS_H
+#define DIC_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_DIC
+
+#include "CPP_OPTIONS.h"
+
+C CPP Macros go here
+#define DIC_BIOTIC
+#undef  ALLOW_FE
+#undef  CAR_DISS
+
+#endif /* ALLOW_DIC */
+#endif /* DIC_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
