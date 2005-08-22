d1dc21f4219ad80f429df923686147b66bd2f39e
Jean-Michel Campin <jmc@ocean.mit.edu>
2005-08-22 23:00:58 +0000

    allow to compile this pkg without pkg MDSIO.

---
+++ pkg/rw/RW_OPTIONS.h
@@ -0,0 +1,20 @@
+C $Header$
+C $Name$
+
+C CPP options file for RW package
+C
+C Use this file for selecting options within the RW package
+
+#ifndef RW_OPTIONS_H
+#define RW_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_RW
+
+#include "CPP_OPTIONS.h"
+
+#endif /* ALLOW_RW */
+#endif /* RW_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
