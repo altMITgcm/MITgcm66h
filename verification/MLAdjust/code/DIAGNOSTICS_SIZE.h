6177f850b1fdd940633271fece8ab7b9036b4920
Baylor Fox-Kemper <baylor@brown.edu>
2005-09-20 21:03:31 +0000

    Adding clean version of mom_calc_visc and viscAhRemax and viscA4Remax params. Oops.

--- verification/MLAdjust/code/DIAGNOSTICS_SIZE.h
+++ verification/MLAdjust/code/DIAGNOSTICS_SIZE.h
@@ -21,7 +21,7 @@ C  and "diagSt_size" (statistics-diags) since values here are deliberately small
       INTEGER    diagSt_size
       PARAMETER( ndiagMax = 500 )
       PARAMETER( numlists = 10, numperlist = 50, numLevels=2*Nr )
-      PARAMETER( numdiags = 25*Nr )
+      PARAMETER( numdiags = 50*Nr )
       PARAMETER( nRegions = 0 , nStats = 4 )
       PARAMETER( diagSt_size = 10*Nr )
 
