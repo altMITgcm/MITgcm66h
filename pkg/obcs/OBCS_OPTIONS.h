50d135d8c47cc6445f0bb8fb16c1401c293d5b59
Martin Losch <Martin.Losch@awi.de>
2005-10-10 05:53:49 +0000

    o OBCS and PTRACERS: add open boundary support for passive tracers   - either use homogenous (pseudo) v.Neumann conditions or prescribe     OB-values from file; this is not different from the way theta and salinity     are treated   - however, Orlanski-radiation conditions are not supported, and the model     will stop if you use pTracers and Orlanski at the same time.   - beefed up the rountine obcs_external_fields_load: now only those open     boundary values are overwritten with values from files for which there     are really files, otherwise the OB-fields remain untouched. This makes     it possible to use different OBs at different ends of the domain (as     with EXF)   - TODO: add support for OB?w and OB?eta, which can currently not be read     from a file.

--- pkg/obcs/OBCS_OPTIONS.h
+++ pkg/obcs/OBCS_OPTIONS.h
@@ -23,14 +23,15 @@ C Enable individual open boundaries
 C This include hooks to the Orlanski Open Boundary Radiation code
 #define ALLOW_ORLANSKI
 
-C Enable OB values to be prescribed via EXF package
+C Enable OB values to be prescribed via external fields that are read
+C from a file
 #undef ALLOW_OBCS_PRESCRIBE
 
 C This includes hooks to sponge layer treatment of uvel, vvel
 #undef ALLOW_OBCS_SPONGE
 
 C balance barotropic velocity
-# undef ALLOW_OBCS_BALANCE
+#undef ALLOW_OBCS_BALANCE
 
 #endif /* ALLOW_OBCS */
 #endif /* OBCS_OPTIONS_H */
