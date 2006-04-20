bfca1c67dbf752689d684d282df78b261ef48e6e
Jean-Michel Campin <jmc@ocean.mit.edu>
2006-04-20 16:34:51 +0000

    tutorial_global_oce_biogeo : copied from dic_example

---
+++ verification/tutorial_global_oce_biogeo/code/DIC_OPTIONS.h
@@ -0,0 +1,18 @@
+C $Header$
+C $Name$
+
+#ifndef DIC_OPTIONS_H
+#define DIC_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_DIC
+
+#include "CPP_OPTIONS.h"
+
+C CPP Macros go here
+
+#endif /* ALLOW_DIC */
+#endif /* DIC_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
