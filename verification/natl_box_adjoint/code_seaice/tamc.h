a33211f83101b26acb2fcf1cf2074f0ed8d0db7f
Patrick Heimbach <heimbach@mit.edu>
2006-05-06 18:02:11 +0000

    Move iicekey.

--- verification/natl_box_adjoint/code_seaice/tamc.h
+++ verification/natl_box_adjoint/code_seaice/tamc.h
@@ -120,7 +120,7 @@ cph      PARAMETER( maxpass     = PTRACERS_num + 2 )
       INTEGER act0, act1, act2, act3, act4
       INTEGER max0, max1, max2, max3
       INTEGER iikey, kkey, passkey, igadkey, 
-     &        itdkey, idynkey, igmkey, ikppkey, iicekey
+     &        itdkey, idynkey, igmkey, ikppkey
 
 c     ================================================================
 c     END OF HEADER TAMC
