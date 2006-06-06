744ac71565ccc667c1b64171690f286ebb5d52ff
Jean-Michel Campin <jmc@ocean.mit.edu>
2006-06-06 22:21:15 +0000

    cvapor_fac_ice was only 1000 too large

--- pkg/exf/exf_constants.h
+++ pkg/exf/exf_constants.h
@@ -149,7 +149,7 @@ c     hq           - height of mean rel. humidity
      &            karman         =        0.400    d0 ,
      &            cvapor_fac     =   640380.000    d0 ,
      &            cvapor_exp     =     5107.400    d0 ,
-     &            cvapor_fac_ice = 11637800.000    d3 ,
+     &            cvapor_fac_ice = 11637800.000    d0 ,
      &            cvapor_exp_ice =     5897.800    d0 ,
      &            humid_fac   =           0.606    d0 ,
      &            gamma_blk   =           0.010    d0 ,
