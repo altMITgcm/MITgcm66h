94214868002632a336ce770ab2300bc1c85a1637
Ed Hill <ed@eh3.com>
2006-08-15 04:05:48 +0000

    initial check-in

---
+++ pkg/regrid/REGRID_OPTIONS.h
@@ -0,0 +1,24 @@
+C $Header$
+C $Name$
+
+C     Use this file to select options within the REGRID package
+
+#ifndef REGRID_OPTIONS_H
+#define REGRID_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_REGRID
+
+#include "CPP_OPTIONS.h"
+
+C     Place CPP define/undef flags here
+
+C     Currently, there are no compile-time options for the REGRID
+C     package.
+
+
+#endif /* ALLOW_REGRID */
+#endif /* REGRID_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
---
+++ pkg/regrid/REGRID_SIZE.h
@@ -0,0 +1,14 @@
+C $Header$
+C $Name$
+      
+      INTEGER    REGRID_NGRID_MAX
+      INTEGER    REGRID_NELEM_MAX
+
+      PARAMETER( REGRID_NGRID_MAX =    10 )
+      PARAMETER( REGRID_NELEM_MAX = 20000 )
+
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
+
