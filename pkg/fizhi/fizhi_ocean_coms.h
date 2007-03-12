aeb2aad80dfff4276c3e64347dd506bc2d73bc41
Andrea Molod <molod@ocean.mit.edu>
2007-03-12 21:33:27 +0000

    Add options for yearly sst data, move some other stuff around a bit

--- pkg/fizhi/fizhi_ocean_coms.h
+++ pkg/fizhi/fizhi_ocean_coms.h
@@ -1,10 +1,15 @@
 C $Header$
 C $Name$
 
+c Ocean Parameters
+c -------------------
+      common /ocean_params/sstclim,sstfreq,siceclim,sicefreq,ksst,kice
+      logical sstclim,sstfreq,siceclim,sicefreq
+      integer ksst, kice
+
 c Ocean Exports
 c -------------------
-      common /ocean_exports/ sst, sice, ksst, kice
+      common /ocean_exports/ sst, sice
       _RL sst(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nsx,Nsy)
       _RL sice(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nsx,Nsy)
-      integer ksst, kice
 
