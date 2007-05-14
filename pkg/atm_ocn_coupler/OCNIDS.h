d2cfb71c5155f0e4d1affe4f8a798a2e75540f80
Jean-Michel Campin <jmc@ocean.mit.edu>
2007-05-14 21:00:44 +0000

    add lines to deliberately cause a compile-time error.

--- pkg/atm_ocn_coupler/OCNIDS.h
+++ pkg/atm_ocn_coupler/OCNIDS.h
@@ -1,11 +1,20 @@
 C $Header$
 C $Name$
 
-C     /==========================================================\
-C     | OCNIDS.h Declare symbolic constants holding strings that |
-C     |          are used to identify this component and the     |
-C     |          fields it exchanges with other components.      |
-C     \==========================================================/
+  These lines are here to deliberately cause a compile-time error.
+  If you see these lines in your .F files or the compiler shows them
+    as an error then it means you have not placed your coupler 
+    configuration files in the appropriate place.
+  You need: a) to place you own copy of OCNIDS.h in a directory
+    (e.g.: shared_code) which is both in the include path of the
+    coupler and in include path of the ocean component ; 
+  and b) to comment out these lines.
+
+C     *==========================================================*
+C     | OCNIDS.h Declare symbolic constants holding strings that
+C     |          are used to identify this component and the
+C     |          fields it exchanges with other components.
+C     *==========================================================*
       CHARACTER*(8)  ocnCompName 
       PARAMETER( ocnCompName         = 'UV-Ocean'       )
 
