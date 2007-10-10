adaf1e7fbffd4571aedd074e91d33034cf14716d
Martin Losch <Martin.Losch@awi.de>
2007-10-10 09:26:39 +0000

    add option for convservative form of Hellmer&Olbers (1989) thermodynamics - requires a little reorganization, which affects the testreport results   of isomip.htd (only 7 digits of cg2d agree) => update experiment   The results are extremely sensitive to details of the algorithm   (e.g., order in which sums and differences are taken, etc) so that   replacing a term with a (theoretically) exactly equal other term   causes the big difference; possibly, because the solution of a   quadratic equation is involed. I am not happy about this, but the   present formulation appears to be a little more robust. - add a few comments

--- pkg/shelfice/SHELFICE.h
+++ pkg/shelfice/SHELFICE.h
@@ -25,6 +25,8 @@ C     SHELFICEsaltTransCoeff   - salinity transfer coefficient that determines
 C                                salt flux into shelfice (m/s)
 C     SHELFICElatentHeat       - latent heat of fusion (J/kg)
 C     useISOMIPTD              - use simple ISOMIP thermodynamics
+C     SHELFICEconserve         - use conservative form of H&O-thermodynamics 
+C                                following Jenkins et al. (2001, JPO)
 C     SHELFICEboundaryLayer    - turn on vertical merging of cells to for a 
 C                                boundary layer of drF thickness
 C     no_slip_shelfice         - set slip conditions for shelfice separately,
@@ -88,6 +90,7 @@ CEOP
       
       LOGICAL SHELFICEisOn
       LOGICAL useISOMIPTD
+      LOGICAL SHELFICEconserve
       LOGICAL SHELFICEboundaryLayer
       LOGICAL no_slip_shelfice
       LOGICAL SHELFICEwriteState
@@ -98,6 +101,7 @@ CEOP
       COMMON /SHELFICE_PARMS_L/
      &     SHELFICEisOn,
      &     useISOMIPTD,
+     &     SHELFICEconserve,
      &     SHELFICEboundaryLayer,
      &     no_slip_shelfice,
      &     SHELFICEwriteState,
