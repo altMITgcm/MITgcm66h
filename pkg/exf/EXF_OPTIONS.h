31f16e7e925205abb2966a0d2f9b27939e2ae738
Patrick Heimbach <heimbach@mit.edu>
2007-12-28 21:26:16 +0000

    Add AD_CONFIG.h

--- pkg/exf/EXF_OPTIONS.h
+++ pkg/exf/EXF_OPTIONS.h
@@ -4,6 +4,8 @@ C $Name$
 #ifndef EXF_OPTIONS_H
 #define EXF_OPTIONS_H
 #include "PACKAGES_CONFIG.h"
+#include "AD_CONFIG.h"
+
 #ifdef ALLOW_EXF
 
 #include "CPP_OPTIONS.h"
@@ -129,7 +131,7 @@ c
 c   ====================================================================
 
 C   Do more printout for the protocol file than usual.
-#define EXF_VERBOSE
+#undef EXF_VERBOSE
 
 C   Bulk formulae related flags.
 #define  ALLOW_ATM_TEMP
