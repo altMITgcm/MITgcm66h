57cfe00243465033b477440edec3e019f1268253
Martin Losch <Martin.Losch@awi.de>
2008-01-16 09:48:41 +0000

    add a new flag SEAICEuseTEM to use a slightly different   ice rheology (so-called truncated ellipse, that gets rid of tensile stress);   for now this is only available for the C-grid code and also within   #ifdef SEAICE_ALLOW_TEM, because this option may slow down the EVP code   default is to not use it.

--- pkg/seaice/SEAICE_OPTIONS.h
+++ pkg/seaice/SEAICE_OPTIONS.h
@@ -69,10 +69,12 @@ C     historical reasons). Define the following flag to use a new
 C     (not thoroughly) test version on a C-grid
 #define SEAICE_CGRID
 
-C--   Only for the C-grid version it is possible to enable EVP code by
-C     defining the following flag
+C--   Only for the C-grid version it is possible to 
 #ifdef SEAICE_CGRID
+C     enable EVP code by defining the following flag
 #define SEAICE_ALLOW_EVP
+C     allow the truncated ellipse rheology (runtime flag SEAICEuseTEM)
+#undef SEAICE_ALLOW_TEM
 #endif /* SEAICE_CGRID */
 
 C--   When set use MAX_HEFF to cap sea ice thickness in seaice_growth
