a98d5975a3e2ea5c1979b6e1f93db3634b8a55b4
Patrick Heimbach <heimbach@mit.edu>
2008-04-19 18:04:44 +0000

    Go to the bar, again.

--- pkg/ctrl/ctrl_dummy.h
+++ pkg/ctrl/ctrl_dummy.h
@@ -65,6 +65,8 @@ c     ==================================================================
      &                  , xx_tauy_mean_dummy
      &                  , xx_hflux_mean_dummy
      &                  , xx_sflux_mean_dummy
+     &                  , xx_sstbar_mean_dummy
+     &                  , xx_sssbar_mean_dummy
      &                  , xx_atemp_mean_dummy
      &                  , xx_aqh_mean_dummy
      &                  , xx_precip_mean_dummy
@@ -137,6 +139,8 @@ c
       _RL xx_bpbar_mean_dummy
       _RL xx_hflux_mean_dummy
       _RL xx_sflux_mean_dummy
+      _RL xx_sstbar_mean_dummy
+      _RL xx_sssbar_mean_dummy
       _RL xx_taux_mean_dummy
       _RL xx_tauy_mean_dummy
       _RL xx_atemp_mean_dummy
