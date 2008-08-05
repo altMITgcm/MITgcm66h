a6b871555bd141b12a16077212dda936a301fae7
Jean-Michel Campin <jmc@ocean.mit.edu>
2008-08-05 21:49:31 +0000

    From Nicolas: new package, (Cheap) Atmospheric Mixed Layer

---
+++ pkg/cheapaml/CHEAPAML_OPTIONS.h
@@ -0,0 +1,22 @@
+C $Header$
+C $Name$
+
+C Options file for CheapAML package
+C
+C Use this file for selecting options within the CheapAML package
+
+#ifndef CHEAPAML_OPTIONS_H
+#define CHEAPAML_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_CHEAPAML
+
+#include "CPP_OPTIONS.h"
+
+C Place CPP define/undef flag here
+
+#endif /* ALLOW_CHEAPAML */
+#endif /* CHEAPAML_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
