39f29aad951afedd6a97148d12b320225d95fdf4
Patrick Heimbach <heimbach@mit.edu>
2008-10-11 20:37:05 +0000

    Make frWtrAtm global for FW balancing in presence of sea-ice, and I need it outside of seaice code. Also move DIAGNOSTICS_FILL(frWtrAtm ... (hope I'm not making a betise).

--- pkg/seaice/SEAICE.h
+++ pkg/seaice/SEAICE.h
@@ -99,6 +99,15 @@ CEOP
       _RL YNEG       (1-OLx:sNx+OLx,1-OLy:sNy+OLy,  nSx,nSy)
       _RL RIVER      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,  nSx,nSy)
 
+C     saltWtrIce contains m of salty ice melted (<0) or created (>0)
+C     frWtrIce contains m of freshwater ice melted (<0) or created (>0)
+C              that is, ice due to precipitation or snow
+C     frWtrAtm contains freshwater flux from the atmosphere
+      COMMON/ICEFLUX/ saltWtrIce, frWtrIce, frWtrAtm
+      _RL saltWtrIce (1-OLx:sNx+OLx,1-OLy:sNy+OLy,  nSx,nSy)
+      _RL frWtrIce   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,  nSx,nSy)
+      _RL frWtrAtm   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,  nSx,nSy)
+
       INTEGER MULTDIM
       PARAMETER (MULTDIM=7)
 #ifdef SEAICE_MULTICATEGORY
