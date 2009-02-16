75b04e9fc98388cdc7345cc1fdabc64ba0513339
Patrick Heimbach <heimbach@mit.edu>
2009-02-16 00:47:13 +0000

    Make isbyte=8 default, update output.

--- verification/lab_sea/code_ad_seaice_ocean/tamc.h
+++ verification/lab_sea/code_ad_seaice_ocean/tamc.h
@@ -103,7 +103,12 @@ c     and writing data.
       integer iloop_daily
 
       INTEGER    isbyte
-      PARAMETER( isbyte    = 4 )
+#ifdef ALLOW_TAMC_SINGLEPREC_COMLEV
+      PARAMETER( isbyte      = 4 )
+#else
+      PARAMETER( isbyte      = 8 )
+#endif
+
       INTEGER    maximpl
       PARAMETER( maximpl   = 6 )
 #ifdef ALLOW_PTRACERS
@@ -120,7 +125,7 @@ cph      PARAMETER( maxpass     = PTRACERS_num + 2 )
       INTEGER act0, act1, act2, act3, act4
       INTEGER max0, max1, max2, max3
       INTEGER iikey, kkey, passkey, igadkey, 
-     &        itdkey, idynkey, igmkey, ikppkey, iicekey
+     &        itdkey, idynkey, igmkey, iicekey
 
 c     ================================================================
 c     END OF HEADER TAMC
