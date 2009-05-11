36c5ed5ebcc3009b6f44b3bbe40fc1580a3794ab
Jean-Michel Campin <jmc@ocean.mit.edu>
2009-05-11 02:20:49 +0000

    move mapping to global io-buffer inside gather_2d/scater_2d ; save memory (1 less 2D global RL array) + only send/receive real*4 arr when 32.bit file

--- pkg/mdsio/MDSIO_SCPU.h
+++ pkg/mdsio/MDSIO_SCPU.h
@@ -3,19 +3,19 @@ C $Name$
 C
 C Buffers for single CPU IO that allow it to work in multi-threaded code
 C
-C     sharedLocalBuf :: Heap storage buffer to which master thread copies
-C                       data (during read) as part of a scatter/gather and
-C                       from which all threads read data (during read).
+C     sharedLocBuf_rx :: Heap storage buffer to which master thread copies
+C                        data (during read) as part of a scatter/gather and
+C                        from which all threads read data (during read).
       INTEGER xyBuffer_size
 #if defined(ALLOW_EXCH2) && !defined(MISSING_TILE_IO)
       PARAMETER ( xyBuffer_size = W2_ioBufferSize )
 #else
       PARAMETER ( xyBuffer_size = Nx*Ny )
 #endif
-      COMMON /MDSIO_SCPU_R/ sharedLocalBuf
-      _RL sharedLocalBuf(1:sNx,1:sNy,nSx,nSy)
+      COMMON /MDSIO_SCPU_R/ sharedLocBuf_r8, sharedLocBuf_r4
+      Real*8 sharedLocBuf_r8(1:sNx,1:sNy,nSx,nSy)
+      Real*4 sharedLocBuf_r4(1:sNx,1:sNy,nSx,nSy)
 
-      COMMON /MDSIO_BUFFERS/ globalBuf, xy_buffer_r8, xy_buffer_r4
-      Real*8 globalBuf(Nx,Ny)
+      COMMON /MDSIO_BUFFERS/ xy_buffer_r8, xy_buffer_r4
       Real*8 xy_buffer_r8(xyBuffer_size)
       Real*4 xy_buffer_r4(xyBuffer_size)
