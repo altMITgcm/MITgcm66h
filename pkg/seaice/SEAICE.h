20003a990ce35738214ba18ffb2f37c29e9b4e25
Dimitris Menemenlis <Dimitris.Menemenlis@jpl.nasa.gov>
2009-06-21 23:08:48 +0000

    o adding JM's doc/diags_changes.txt:   a very useful summary that I refer to all the time o adding pkg/seaice diagnostics for atmospheric and oceanic    heat flux and for atmospheric and oceanic ice growth

--- pkg/seaice/SEAICE.h
+++ pkg/seaice/SEAICE.h
@@ -130,11 +130,9 @@ C
 C     saltWtrIce contains m of salty ice melted (<0) or created (>0)
 C     frWtrIce contains m of freshwater ice melted (<0) or created (>0)
 C              that is, ice due to precipitation or snow
-C     frWtrAtm contains freshwater flux from the atmosphere
-      COMMON/ICEFLUX/ saltWtrIce, frWtrIce, frWtrAtm
+      COMMON/ICEFLUX/ saltWtrIce, frWtrIce
       _RL saltWtrIce (1-OLx:sNx+OLx,1-OLy:sNy+OLy,  nSx,nSy)
       _RL frWtrIce   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,  nSx,nSy)
-      _RL frWtrAtm   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,  nSx,nSy)
 
       INTEGER MULTDIM
       PARAMETER (MULTDIM=7)
