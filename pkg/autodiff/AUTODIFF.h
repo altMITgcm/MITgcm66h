edc7ba5494905d9e7ff954f797527186dd6c74ff
Patrick Heimbach <heimbach@mit.edu>
2009-06-24 20:09:08 +0000

    Adjust store blocks following change of 3D to 2D arrays

--- pkg/autodiff/AUTODIFF.h
+++ pkg/autodiff/AUTODIFF.h
@@ -15,7 +15,7 @@ C $Name$
       PARAMETER (NEXF2  = 18)
       PARAMETER (NCTRL1 = 18)
       PARAMETER (NOB = 18)
-      PARAMETER (NSI = 27)	
+      PARAMETER (NSI = 19)	
       _RL StoreDynVars3D
      &    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy,NDV3D)
       _RL StoreDynVars2D
