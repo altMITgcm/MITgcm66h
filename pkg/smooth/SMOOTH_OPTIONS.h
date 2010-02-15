0dc0d64865d14041d64f04d1c0cbaf3622522ae5
Gael Forget <gforget@mit.edu>
2010-02-15 23:46:04 +0000

    smooth package -- going to the main repository

---
+++ pkg/smooth/SMOOTH_OPTIONS.h
@@ -0,0 +1,20 @@
+C $Header$
+C $Name$
+
+C CPP options file for SMOOTH package
+C
+C Use this file for selecting options within the SMOOTH package
+
+#ifndef SMOOTH_OPTIONS_H
+#define SMOOTH_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_SMOOTH
+
+#include "CPP_OPTIONS.h"
+
+#endif /* ALLOW_SMOOTH */
+#endif /* SMOOTH_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
