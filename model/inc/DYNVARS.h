d6a3cd53650cb99b06ae57113d9c88d1c42223aa
Gael Forget <gforget@mit.edu>
2010-02-17 00:21:35 +0000

    ALLOW_3D_VISCAH/ALLOW_3D_VISCA4 => 3D specification of Viscosity    => addition to the viscosity field as computed by others schemes

--- model/inc/DYNVARS.h
+++ model/inc/DYNVARS.h
@@ -78,6 +78,24 @@ C               for mixing of tracers vertically ( units of r^2/s )
       _RL  diffKr (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
 #endif
 
+#ifdef ALLOW_3D_VISCAH
+C     viscAhDfld, viscAhZfld :: full 3D specification of Laplacian Viscosity 
+C               coeff. for mixing of momentum horizontally ( units of r^2/s )
+      COMMON /DYNVARS_3D_VISCAH/
+     &                  viscAhDfld, viscAhZfld
+      _RL  viscAhDfld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL  viscAhZfld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+#endif
+
+#ifdef ALLOW_3D_VISCA4
+C     viscA4Dfld, viscA4Zfld :: full 3D specification of Bi-harmonic Viscosity
+C               coeff. for mixing of momentum horizontally ( units of r^2/s )
+      COMMON /DYNVARS_3D_VISCA4/
+     &                  viscA4Dfld, viscA4Zfld
+      _RL  viscA4Dfld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL  viscA4Zfld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+#endif
+
 cph(
 cph the following block will eventually move to a separate
 cph header file containing requires anomaly fields of control vars.
