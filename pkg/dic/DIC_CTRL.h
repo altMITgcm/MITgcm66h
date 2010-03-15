f0ccabe9e7c32da63b23fdc02ada36409518e251
Jean-Michel Campin <jmc@ocean.mit.edu>
2010-03-15 22:55:51 +0000

    add CVS Header & Name

--- pkg/dic/DIC_CTRL.h
+++ pkg/dic/DIC_CTRL.h
@@ -1,11 +1,13 @@
-C     /==========================================================\
-C     | DIC_XX.h                                                 |
-C     | o Control of Biological Carbon Variables                 |
-C     |==========================================================|
+C $Header$
+C $Name$
+
+C     *==========================================================*
+C     | DIC_XX.h
+C     | o Control of Biological Carbon Variables
+C     *==========================================================*
 
       integer    dic_n_control
       parameter( dic_n_control = 5 )
       COMMON /DIC_XX_R/ xx_dic
       _RL               xx_dic(dic_n_control)
-
 
