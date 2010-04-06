abffc3477f009861a38f24c2fe32dd118133012f
Jean-Michel Campin <jmc@ocean.mit.edu>
2010-04-06 20:38:18 +0000

    clean-up RBCS code: add RBCS_OPTIONS.h file ; fix multi-threaded ;   change (+ fix IO calls) type of array RBC_mask from _RL to _RS.

---
+++ pkg/rbcs/RBCS_OPTIONS.h
@@ -0,0 +1,22 @@
+C $Header$
+C $Name$
+
+C CPP options file for pkg RBCS
+C
+C Use this file for selecting options within package "RBCS"
+
+#ifndef RBCS_OPTIONS_H
+#define RBCS_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_RBCS
+
+#include "CPP_OPTIONS.h"
+
+C Place CPP define/undef flag here
+
+#endif /* ALLOW_RBCS */
+#endif /* RBCS_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
