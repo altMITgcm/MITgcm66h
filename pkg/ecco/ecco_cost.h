466a706aff6af4a5742dbdfb68c80f42918e26ad
Gael Forget <gforget@mit.edu>
2010-08-28 18:35:16 +0000

    - Introducing gencost_name that will be used when printing cost function to file. - Changing default for gencost_barfile to ' ' in which case no specific bar file is generated.

--- pkg/ecco/ecco_cost.h
+++ pkg/ecco/ecco_cost.h
@@ -85,12 +85,14 @@ c     objf_gencost - gencost user defined contribution
       integer gencost_nrec(NGENCOST)
 
       common /ecco_gencost_c/
+     &       gencost_name,
      &       gencost_scalefile,
      &       gencost_errfile,
      &       gencost_datafile,
      &       gencost_barfile,
      &       gencost_avgperiod,
      &       gencost_mask
+      character*(MAX_LEN_FNAM) gencost_name(NGENCOST)
       character*(MAX_LEN_FNAM) gencost_scalefile(NGENCOST)
       character*(MAX_LEN_FNAM) gencost_errfile(NGENCOST)
       character*(MAX_LEN_FNAM) gencost_datafile(NGENCOST)
