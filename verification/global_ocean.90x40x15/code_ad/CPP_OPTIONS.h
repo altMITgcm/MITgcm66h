9b6a200848187bc24d8fd549ac66112b0f8f58c5
Jean-Michel Campin <jmc@ocean.mit.edu>
2010-09-11 21:34:50 +0000

    add #define DISABLE_SIGMA_CODE (no need to bother with sigma-coordinate for now)

--- verification/global_ocean.90x40x15/code_ad/CPP_OPTIONS.h
+++ verification/global_ocean.90x40x15/code_ad/CPP_OPTIONS.h
@@ -15,6 +15,7 @@ C o Nonlinear free-surface code (but without rStar)
 #define EXACT_CONSERV
 #define NONLIN_FRSURF
 #define DISABLE_RSTAR_CODE
+#define DISABLE_SIGMA_CODE
 C o Include/exclude Implicit vertical advection code
 #undef INCLUDE_IMPLVERTADV_CODE
 cph)
