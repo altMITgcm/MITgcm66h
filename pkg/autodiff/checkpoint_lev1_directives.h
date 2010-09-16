317f7bbf1fdc27963113709a84b3afde90776b0a
Patrick Heimbach <heimbach@mit.edu>
2010-09-16 16:02:42 +0000

    Enabling (perhaps) AB3 + NLFS + RSTAR initial tests for ECCO-GODAE v4 and global_ocean.cs

--- pkg/autodiff/checkpoint_lev1_directives.h
+++ pkg/autodiff/checkpoint_lev1_directives.h
@@ -57,8 +57,10 @@ CADJ &     kind = isbyte
 # else
 CADJ STORE gsnm,gtnm          = comlev1, key = ikey_dynamics,
 CADJ &     kind = isbyte
-CADJ STORE gunm,gvnm          = comlev1, key = ikey_dynamics,
-CADJ &     kind = isbyte
+cphCADJ STORE gunm,gvnm          = comlev1, key = ikey_dynamics,
+cphCADJ &     kind = isbyte
+CADJ STORE rstardhcdt,rstardhsdt,rstardhwdt 
+CADJ &     = comlev1, key = ikey_dynamics, kind = isbyte
 # endif
 CADJ STORE salt,theta         = comlev1, key = ikey_dynamics,
 CADJ &     kind = isbyte
