0206464719d16f97de2219088849233a672362e2
Gael Forget <gforget@mit.edu>
2010-10-07 21:17:48 +0000

    revision of lab_sea forward model tests ======================================= o clean-up of lab_sea code directory. o addition of SEAICE_VARIABLE_FREEZING_POINT.

--- verification/lab_sea/code/DIAGNOSTICS_SIZE.h
+++ verification/lab_sea/code/DIAGNOSTICS_SIZE.h
@@ -8,16 +8,16 @@ C     ndiagMax   :: maximum total number of available diagnostics
 C     numlists   :: maximum number of diagnostics list (in data.diagnostics)
 C     numperlist :: maximum number of active diagnostics per list (data.diagnostics)
 C     numLevels  :: maximum number of levels to write    (data.diagnostics)
-C     numdiags   :: maximum size of the storage array for active 2D/3D diagnostics
+C     numDiags   :: maximum size of the storage array for active 2D/3D diagnostics
 C     nRegions   :: maximum number of regions (statistics-diagnostics)
 C     sizRegMsk  :: maximum size of the regional-mask (statistics-diagnostics)
 C     nStats     :: maximum number of statistics (e.g.: aver,min,max ...)
 C     diagSt_size:: maximum size of the storage array for statistics-diagnostics
-C Note : may need to increase "numdiags" when using several 2D/3D diagnostics,
+C Note : may need to increase "numDiags" when using several 2D/3D diagnostics,
 C  and "diagSt_size" (statistics-diags) since values here are deliberately small.
       INTEGER    ndiagMax
       INTEGER    numlists, numperlist, numLevels
-      INTEGER    numdiags
+      INTEGER    numDiags
       INTEGER    nRegions, sizRegMsk, nStats
       INTEGER    diagSt_size
       PARAMETER( ndiagMax = 500 )
