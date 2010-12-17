013dd65f0cfa0897aa98376bec525da94c35c7da
Gael Forget <gforget@mit.edu>
2010-12-17 04:00:14 +0000

    rename iicekey as ticekey to avoid conflict with pkg/seaice

--- pkg/thsice/THSICE_SIZE.h
+++ pkg/thsice/THSICE_SIZE.h
@@ -25,6 +25,13 @@ C--   identifiers for advected properties
      &            GAD_SI_QICE1 = -8,
      &            GAD_SI_QICE2 = -9 )
 
+#ifdef ALLOW_AUTODIFF_TAMC
+      INTEGER ticekey
+      INTEGER ikey_1, ikey_2, ikey_3, ikey_4
+      INTEGER MaxTsf
+      PARAMETER ( MaxTsf = 20 )
+#endif
+
 #endif /* ALLOW_THSICE */
 
 CEH3 ;;; Local Variables: ***
