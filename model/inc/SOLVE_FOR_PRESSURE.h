9f92a04280945bd7d293355b80ac11c88abf6332
Jean-Michel Campin <jmc@ocean.mit.edu>
2011-02-22 18:32:28 +0000

    comment out common block to test/check that EXCH works in multi-threaded when applied to local variable (cg2d_x).

--- model/inc/SOLVE_FOR_PRESSURE.h
+++ model/inc/SOLVE_FOR_PRESSURE.h
@@ -12,6 +12,6 @@ C     | o Globals used by Fortran pressure solver routine
 C     *==========================================================*
 C     \ev
 CEOP
-      COMMON / SFP_COMMON_R8 / cg2d_x, cg2d_b
+c     COMMON / SFP_COMMON_R8 / cg2d_x, cg2d_b
       _RL cg2d_x(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL cg2d_b(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
