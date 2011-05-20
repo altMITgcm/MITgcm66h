afd1b5a1927d039246854141862f1437ad333a98
Gael Forget <gforget@mit.edu>
2011-05-20 00:45:59 +0000

    - avoid recomputation

--- pkg/rbcs/rbcs_ad_check_lev2_dir.h
+++ pkg/rbcs/rbcs_ad_check_lev2_dir.h
@@ -6,6 +6,13 @@ CADJ STORE rbct1 = tapelev2, key = ilev_2
 CADJ STORE rbcs0 = tapelev2, key = ilev_2
 CADJ STORE rbcs1 = tapelev2, key = ilev_2
 
+#ifndef DISABLE_RBCS_MOM
+CADJ STORE rbcu0 = tapelev2, key = ilev_2
+CADJ STORE rbcu1 = tapelev2, key = ilev_2
+CADJ STORE rbcv0 = tapelev2, key = ilev_2
+CADJ STORE rbcv1 = tapelev2, key = ilev_2
+#endif
+
 #ifdef ALLOW_PTRACERS
 CADJ STORE rbcptr0 = tapelev2, key = ilev_2
 CADJ STORE rbcptr1 = tapelev2, key = ilev_2
--- pkg/rbcs/rbcs_ad_check_lev4_dir.h
+++ pkg/rbcs/rbcs_ad_check_lev4_dir.h
@@ -6,6 +6,13 @@ CADJ STORE rbct1 = tapelev4, key = ilev_4
 CADJ STORE rbcs0 = tapelev4, key = ilev_4
 CADJ STORE rbcs1 = tapelev4, key = ilev_4
 
+#ifndef DISABLE_RBCS_MOM
+CADJ STORE rbcu0 = tapelev4, key = ilev_4
+CADJ STORE rbcu1 = tapelev4, key = ilev_4
+CADJ STORE rbcv0 = tapelev4, key = ilev_4
+CADJ STORE rbcv1 = tapelev4, key = ilev_4
+#endif
+
 #ifdef ALLOW_PTRACERS
 CADJ STORE rbcptr0 = tapelev4, key = ilev_4
 CADJ STORE rbcptr1 = tapelev4, key = ilev_4
