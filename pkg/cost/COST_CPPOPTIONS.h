97ec700c06788f72bf85ddbb762ccb81a2d47892
Jean-Michel Campin <jmc@ocean.mit.edu>
2011-05-24 17:09:19 +0000

    like standard {PKG}_OPTIONS.h file (but with AD_CONFIG.h)

--- pkg/cost/COST_CPPOPTIONS.h
+++ pkg/cost/COST_CPPOPTIONS.h
@@ -1,8 +1,29 @@
 C $Header$
 C $Name$
 
+CBOP
+C !ROUTINE: COST_CPPOPTIONS.h
+C !INTERFACE:
+C #include "COST_CPPOPTIONS.h"
+
+C !DESCRIPTION:
+C *==================================================================*
+C | CPP options file for Cost-Function (cost) package:
+C | Control which optional features to compile in this package code.
+C *==================================================================*
+CEOP
+
+#ifndef COST_OPTIONS_H
+#define COST_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "AD_CONFIG.h"
+#ifdef ALLOW_COST
+
 #include "CPP_OPTIONS.h"
-C
-C CPP flags controlling which code is included in the files that
-C will be compiled.
-C
+
+C-- Package-specific options go here
+C   Note: most of these options have been shifted to the common header
+C         file ECCO_CPPOPTIONS.h
+
+#endif /* ALLOW_COST */
+#endif /* COST_OPTIONS_H */
--- pkg/grdchk/GRDCHK_OPTIONS.h
+++ pkg/grdchk/GRDCHK_OPTIONS.h
@@ -2,22 +2,22 @@ C $Header$
 C $Name$
 
 CBOP
-C !ROUTINE: CTRL_CPPOPTIONS.h
+C !ROUTINE: GRDCHK_OPTIONS.h
 C !INTERFACE:
-C #include "CTRL_CPPOPTIONS.h"
+C #include "GRDCHK_OPTIONS.h"
 
 C !DESCRIPTION:
 C *==================================================================*
-C | CPP options file for Control (ctrl) package:
+C | CPP options file for Gradient-Check (grdchk) package:
 C | Control which optional features to compile in this package code.
 C *==================================================================*
 CEOP
 
-#ifndef CTRL_OPTIONS_H
-#define CTRL_OPTIONS_H
+#ifndef GRDCHK_OPTIONS_H
+#define GRDCHK_OPTIONS_H
 #include "PACKAGES_CONFIG.h"
 #include "AD_CONFIG.h"
-#ifdef ALLOW_CTRL
+#ifdef ALLOW_GRDCHK
 
 #include "CPP_OPTIONS.h"
 
@@ -25,5 +25,5 @@ C-- Package-specific options go here
 C   Note: most of these options have been shifted to the common header
 C         file ECCO_CPPOPTIONS.h
 
-#endif /* ALLOW_CTRL */
-#endif /* CTRL_OPTIONS_H */
+#endif /* ALLOW_GRDCHK */
+#endif /* GRDCHK_OPTIONS_H */
