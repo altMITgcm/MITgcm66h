c1610856f6c662e86e3d01f498e157f880d1ce8e
Patrick Heimbach <heimbach@mit.edu>
2011-08-06 15:42:33 +0000

    Moving test to multi-threaded. The gradient check was moving across tile boundaries, so a different starting index set is chosen to remain within the tile. Reference output changed accordingly (after testing against single-tile).

--- verification/offline_exf_seaice/code_ad/SIZE.h
+++ verification/offline_exf_seaice/code_ad/SIZE.h
@@ -44,12 +44,12 @@ C     Nr  :: No. points in Z for full process domain.
       INTEGER Ny
       INTEGER Nr
       PARAMETER (
-     &           sNx =  20,
-     &           sNy =  16,
+     &           sNx =  10,
+     &           sNy =   8,
      &           OLx =   3,
      &           OLy =   3,
-     &           nSx =   1,
-     &           nSy =   1,
+     &           nSx =   2,
+     &           nSy =   2,
      &           nPx =   1,
      &           nPy =   1,
      &           Nx  = sNx*nSx*nPx,
