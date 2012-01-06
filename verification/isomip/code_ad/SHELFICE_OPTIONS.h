dd8228354907891741271cd9e2c5891ec9fe00c0
Patrick Heimbach <heimbach@mit.edu>
2012-01-06 13:01:11 +0000

    New verif. for adjoint shelfice code

---
+++ verification/isomip/code_ad/SHELFICE_OPTIONS.h
@@ -0,0 +1,29 @@
+C $Header$
+C $Name$
+
+C     /==========================================================\
+C     | SHELFICE_OPTIONS.h                                       |
+C     | o CPP options file for SHELFICE package.                 |
+C     |==========================================================|
+C     | Use this file for selecting options within the SHELFICE  |
+C     | package.                                                 |
+C     \==========================================================/
+
+#ifndef SHELFICE_OPTIONS_H
+#define SHELFICE_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+
+#ifdef ALLOW_SHELFICE
+
+#include "CPP_OPTIONS.h"
+
+C     allow code for simple ISOMIP thermodynamics
+#define ALLOW_ISOMIP_TD
+
+C     allow friction velocity-dependent transfer coefficient
+C     following Holland and Jenkins, JPO, 1999
+#undef SHI_ALLOW_GAMMAFRICT
+
+#endif /* ALLOW_SHELFICE */
+#endif /* SHELFICE_OPTIONS_H */
+
