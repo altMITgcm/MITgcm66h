2dea36f8f4743d2f683e7cc7be314fccf7add05d
Jean-Michel Campin <jmc@ocean.mit.edu>
2012-01-20 01:26:15 +0000

    - derive OB indices in overlap region from OB-InsideMask (in obcs_init_fixed.F)   and remove code (in obcs_readparms.F) which was based on EXCH of indices   (through buffer) and did not account for exch2 (e.g., CS-grid) topology.

--- pkg/obcs/OBCS_GRID.h
+++ pkg/obcs/OBCS_GRID.h
@@ -22,6 +22,7 @@ CEOP
 C tileHasOB[N,S,E,W] :: this tile has OB at Northern/Southern/Eastern/Western edge
 C   OB_Jn, OB_Js     :: indices of Northern & Southern OB location (cell center pt)
 C   OB_Ie, OB_Iw     :: indices of  Eastern & Western  OB location (cell center pt)
+C OBCS_indexStatus   :: track status of OB indices setting
 C OBCS_insideMask    :: Inside OB region mask (zero beyond OB).
 
       COMMON /OBCS_ACTIVE_TILES/
@@ -32,11 +33,13 @@ C OBCS_insideMask    :: Inside OB region mask (zero beyond OB).
       LOGICAL tileHasOBW(nSx,nSy)
 
       COMMON /OBCS_GRID_INDICES/
-     &  OB_Jn, OB_Js, OB_Ie, OB_Iw
-      INTEGER OB_Jn(1-Olx:sNx+Olx,nSx,nSy)
-      INTEGER OB_Js(1-Olx:sNx+Olx,nSx,nSy)
-      INTEGER OB_Ie(1-Oly:sNy+Oly,nSx,nSy)
-      INTEGER OB_Iw(1-Oly:sNy+Oly,nSx,nSy)
+     &  OB_Jn, OB_Js, OB_Ie, OB_Iw,
+     &  OBCS_indexStatus
+      INTEGER OB_Jn(1-OLx:sNx+OLx,nSx,nSy)
+      INTEGER OB_Js(1-OLx:sNx+OLx,nSx,nSy)
+      INTEGER OB_Ie(1-OLy:sNy+OLy,nSx,nSy)
+      INTEGER OB_Iw(1-OLy:sNy+OLy,nSx,nSy)
+      INTEGER OBCS_indexStatus
 
       COMMON /OBCS_MASK_RS/
      &  OBCS_insideMask
