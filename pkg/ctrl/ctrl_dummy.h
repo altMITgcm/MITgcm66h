7077a24c94895dbbf9a78564223a5b52d5152776
Patrick Heimbach <heimbach@mit.edu>
2012-04-19 20:01:29 +0000

    Add new control variables to separate between mean and anomalies.

--- pkg/ctrl/ctrl_dummy.h
+++ pkg/ctrl/ctrl_dummy.h
@@ -69,6 +69,7 @@ c     ==================================================================
      &                  , xx_sflux_mean_dummy
      &                  , xx_sstbar_mean_dummy
      &                  , xx_sssbar_mean_dummy
+     &                  , xx_atmfwbar_mean_dummy
      &                  , xx_atemp_mean_dummy
      &                  , xx_aqh_mean_dummy
      &                  , xx_precip_mean_dummy
@@ -154,6 +155,7 @@ c
       _RL xx_sflux_mean_dummy
       _RL xx_sstbar_mean_dummy
       _RL xx_sssbar_mean_dummy
+      _RL xx_atmfwbar_mean_dummy
       _RL xx_taux_mean_dummy
       _RL xx_tauy_mean_dummy
       _RL xx_atemp_mean_dummy
