779981541c459a0972bb955390e695d4289db6f3
Jean-Michel Campin <jmc@ocean.mit.edu>
2012-07-17 18:02:47 +0000

    increase numItersMax from 100 to 200 (needed with longer deltaTFreeSurf)

--- verification/tutorial_tracer_adjsens/code_ad/tamc.h
+++ verification/tutorial_tracer_adjsens/code_ad/tamc.h
@@ -128,7 +128,7 @@ cph      PARAMETER( maxpass     = PTRACERS_num + 2 )
 
       INTEGER act0, act1, act2, act3, act4
       INTEGER max0, max1, max2, max3
-      INTEGER iikey, kkey, passkey, igadkey, 
+      INTEGER iikey, kkey, passkey, igadkey,
      &        itdkey, idynkey, igmkey
 
 #ifdef ALLOW_CG2D_NSA
@@ -136,11 +136,9 @@ C     Parameter that is needed for the tape complev_cg2d_iter
 C     cannot be smaller than the allowed number of iterations in cg2d
 C     (numItersMax >= cg2dMaxIters in data-file)
       INTEGER numItersMax
-      PARAMETER ( numItersMax = 100 )
+      PARAMETER ( numItersMax = 200 )
 #endif /* ALLOW_CG2D_NSA */
 
 c     ================================================================
 c     END OF HEADER TAMC
 c     ================================================================
-
-
