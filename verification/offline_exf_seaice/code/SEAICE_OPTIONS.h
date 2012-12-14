a53421ecaca8ae139eef20925c8534c8acc94575
Martin Losch <Martin.Losch@awi.de>
2012-12-14 23:23:31 +0000

    define SEAICE_ZETA_SMOOTHREG for smoother convergence and hopefully better agreement between compilers for JFNK; changes results of dynamical solvers

--- verification/offline_exf_seaice/code/SEAICE_OPTIONS.h
+++ verification/offline_exf_seaice/code/SEAICE_OPTIONS.h
@@ -95,7 +95,7 @@ C     from below and above in seaice_evp: not necessary, and not recommended
 C     regularize zeta to zmax with a smooth tanh-function instead
 C     of a min(zeta,zmax). This improves convergence of iterative
 C     solvers (Lemieux and Tremblay 2009, JGR). No effect on EVP
-# undef SEAICE_ZETA_SMOOTHREG
+# define SEAICE_ZETA_SMOOTHREG
 C     allow the truncated ellipse rheology (runtime flag SEAICEuseTEM)
 # undef SEAICE_ALLOW_TEM
 #else /* not SEAICE_CGRID, but old B-grid */
