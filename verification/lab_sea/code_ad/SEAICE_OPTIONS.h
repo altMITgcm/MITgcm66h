86be00b48eba367457d36c06bbcc148a6c894a5b
Gael Forget <gforget@mit.edu>
2012-12-27 23:01:02 +0000

    - remove documentation for retired CPP flags (SEAICE_GROWTH_LEGACY, SEAICE_CAP_HEFF).

--- verification/lab_sea/code_ad/SEAICE_OPTIONS.h
+++ verification/lab_sea/code_ad/SEAICE_OPTIONS.h
@@ -45,10 +45,6 @@ C--   run with sea Ice Thickness Distribution (ITD);
 C     set number of categories (nITD) in SEAICE_SIZE.h
 #undef SEAICE_ITD
 
-C--   Use the Old version of seaice_growth (close to cvs version 1.70)
-C     otherwise, use the merged version (with some of Ian Fenty s code)
-# undef SEAICE_GROWTH_LEGACY
-
 C--   Since the missing sublimation term is now included
 C     this flag is needed for backward compatibility
 #define SEAICE_DISABLE_SUBLIM
@@ -109,9 +105,6 @@ C     It is here for completeness, but its usefulness is unclear.
 # undef SEAICE_LSRBNEW
 #endif /* SEAICE_CGRID */
 
-C--   When set use MAX_HEFF to cap seaice thickness in seaice_growth;
-C     currently only relevant for SEAICE_GROWTH_LEGACY
-#undef SEAICE_CAP_HEFF
 C--   When set limit the Ice-Loading to mass of 1/5 of Surface ocean grid-box
 #undef SEAICE_CAP_ICELOAD
 C--   When set use SEAICE_clipVelocties = .true., to clip U/VICE at 40cm/s,
