d7fd68759c9b9d15334cd2779b4c3428981f3390
Dimitris Menemenlis <Dimitris.Menemenlis@jpl.nasa.gov>
2013-10-25 17:31:43 +0000

    increasing size of max_lat_inc to accomodate ecmwf operational analysis

--- pkg/exf/EXF_PARAM.h
+++ pkg/exf/EXF_PARAM.h
@@ -686,9 +686,10 @@ C  buffer size currently set to 65000 (allows to read-in a 1x1 global data set)
 C  Increase to 140000 to accommodate for ECMWF-INTERIM
       INTEGER    exf_interp_bufferSize
       PARAMETER( exf_interp_bufferSize = 140000 )
-c for lat interpolation, arraysize currently set to 360 max data values
+c for lat interpolation, arraysize currently set to 1279 max data values
+c to accomodate ECMWF operational analysis
       INTEGER MAX_LAT_INC
-      PARAMETER(MAX_LAT_INC = 360)
+      PARAMETER(MAX_LAT_INC = 1279)
 
 C-- Interpolation parameters (for each input field):
 C  {inputField}_lon0    :: longitude of the 1rst point (South-East corner)
