8b0deb68ae1e9d5946d234ed1ceb767fdfb6516d
Jean-Michel Campin <jmc@ocean.mit.edu>
2013-12-02 22:03:09 +0000

    - move some input parameters from CPL_MAP2GRIDS.h to CPL_PARAMS.h   (no need to include CPL_MAP2GRIDS.h in cpl_read_params.F anymore) - report set-up to clog file (I/O unit now passed as input argument) - check we don't go over RunOff map size (=ROsize) and skip reading   grid-cell area if no RunOff map to set.

--- pkg/atm_ocn_coupler/CPL_PARAMS.h
+++ pkg/atm_ocn_coupler/CPL_PARAMS.h
@@ -5,9 +5,22 @@ C     *==========================================================*
 C     | CPL_PARAMS.h
 C     |  - parameter for the Coupler, holds in common block
 C     *==========================================================*
-      
-C--   COMMON /CPL_PAR_I/: Integer parameters
+
+C--   COMMON /CPL_PAR_I/ Integer parameters
+C     runOffMapSize    :: Nunber of connected grid points in RunOff-Map
 C     nCouplingSteps   :: Number of coupling steps
+C     nSteps_atm       :: Number of coupling steps done by ATM
+C     nSteps_ocn       :: Number of coupling steps done by OCN
       COMMON /CPL_PAR_I/
-     &     nCouplingSteps
+     &     runOffMapSize,
+     &     nCouplingSteps,
+     &     nSteps_atm, nSteps_ocn
+      INTEGER runOffMapSize
       INTEGER nCouplingSteps
+      INTEGER nSteps_atm, nSteps_ocn
+
+C--   COMMON /CPL_PAR_C/ Character parameters
+C     runOffMapFile    :: Input file for setting runoffmap
+      COMMON /CPL_PAR_C/ runOffMapFile
+      CHARACTER*80 runOffMapFile
+
