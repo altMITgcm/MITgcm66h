4e03361a67ff1ee8ee544a722406a24584282ba2
Patrick Heimbach <heimbach@mit.edu>
2013-12-17 19:08:34 +0000

    Committing starting setup using OpenAD (incomplete).

---
+++ verification/halfpipe_streamice/code_oad/CTRL_SIZE.h
@@ -0,0 +1,26 @@
+C $Header$
+C $Name$
+
+c     ==================================================================
+c     CTRL_SIZE.h
+c     ==================================================================
+
+C     Generic control variable array dimension
+C     ----------------------------------------
+C
+C     maxCtrlArr2D :: number of 2-d generic init. ctrl variables
+C     maxCtrlArr3D :: number of 3-d generic init. ctrl variables
+C     maxCtrlTim2D :: number of 2-d generic tim-varying ctrl variables
+
+      integer     maxCtrlArr2D
+      parameter ( maxCtrlArr2D = 2 )
+
+      integer     maxCtrlArr3D
+      parameter ( maxCtrlArr3D = 1 )
+
+      integer     maxCtrlTim2D
+      parameter ( maxCtrlTim2D = 1 )
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
