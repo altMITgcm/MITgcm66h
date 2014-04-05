00b9f28cb3b07c25655fe2907e277f46e4ccbaac
Jean-Michel Campin <jmc@ocean.mit.edu>
2014-04-05 21:47:39 +0000

    do not use ECCO_CPPOPTIONS.h anymore for this TAF Adjoint experiment.

--- verification/hs94.1x64x5/code_ad/CPP_OPTIONS.h
+++ verification/hs94.1x64x5/code_ad/CPP_OPTIONS.h
@@ -46,6 +46,9 @@ C o Include/exclude AdamsBashforth-3rd-Order code
 C o Include/exclude nonHydrostatic code
 #undef ALLOW_NONHYDROSTATIC
 
+C o Allow to account for heating due to friction (and momentum dissipation)
+#undef ALLOW_FRICTION_HEATING
+
 C o Allow mass source or sink of Fluid in the interior
 C   (3-D generalisation of oceanic real-fresh water flux)
 #undef ALLOW_ADDFLUID
@@ -121,8 +124,8 @@ C   each of the above pkg get its own options from its specific option file.
 C   Although this method, inherited from ECCO setup, has been traditionally
 C   used for all adjoint built, work is in progress to allow to use the
 C   standard method also for adjoint built.
-#ifdef PACKAGES_CONFIG_H
-# include "ECCO_CPPOPTIONS.h"
-#endif
+c#ifdef PACKAGES_CONFIG_H
+c# include "ECCO_CPPOPTIONS.h"
+c#endif
 
 #endif /* CPP_OPTIONS_H */
