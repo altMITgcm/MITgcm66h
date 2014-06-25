c01ce1a2555df3c3212898831f04e2798858a5aa
Martin Losch <Martin.Losch@awi.de>
2014-06-25 10:48:26 +0000

    move definition of MPSEUDOTIMESTEPS into TAMC-block in SEAICE_SIZE.h

--- pkg/seaice/SEAICE_SIZE.h
+++ pkg/seaice/SEAICE_SIZE.h
@@ -39,7 +39,9 @@ C-    Maximum Number of tracers
       PARAMETER ( NMAX_TICE=10 )
       INTEGER SOLV_MAX_FIXED
       PARAMETER ( SOLV_MAX_FIXED=500 )
-#endif
+      INTEGER MPSEUDOTIMESTEPS
+      PARAMETER (MPSEUDOTIMESTEPS=2)
+#endif /* ALLOW_AUTODIFF_TAMC */
 
 #endif /* ALLOW_SEAICE */
 
