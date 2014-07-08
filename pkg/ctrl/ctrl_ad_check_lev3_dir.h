b0e04cccbea2d1a9420c76ecc6ec09676d0668fd
Jean-Michel Campin <jmc@ocean.mit.edu>
2014-07-08 18:58:41 +0000

    add CVS Header & Name

--- pkg/ctrl/ctrl_ad_check_lev3_dir.h
+++ pkg/ctrl/ctrl_ad_check_lev3_dir.h
@@ -1,7 +1,8 @@
+C $Header$
+C $Name$
 
 #ifdef ALLOW_GENTIM2D_CONTROL
 CADJ STORE xx_gentim2d  = tapelev3, key = ilev_3
 CADJ STORE xx_gentim2d0 = tapelev3, key = ilev_3
 CADJ STORE xx_gentim2d1 = tapelev3, key = ilev_3
 #endif
-
