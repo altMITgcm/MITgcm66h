a8ee1be9699356784b396963d1666135397bbdf7
Jean-Michel Campin <jmc@ocean.mit.edu>
2014-09-09 23:36:57 +0000

    remove "#undef ALLOW_AUTODIFF_OPENAD" (no longer used anywhere)

--- verification/halfpipe_streamice/code_ad/ECCO_CPPOPTIONS.h
+++ verification/halfpipe_streamice/code_ad/ECCO_CPPOPTIONS.h
@@ -15,37 +15,35 @@ C ********************************************************************
 C o Include/exclude code in order to be able to automatically
 C   differentiate the MITgcmUV by using the Tangent Linear and
 C   Adjoint Model Compiler (TAMC).
-C
 #define ALLOW_AUTODIFF_TAMC
+
 C       >>> Checkpointing as handled by TAMC
 #define ALLOW_TAMC_CHECKPOINTING
-C
-#undef ALLOW_AUTODIFF_OPENAD
+
 C       >>> Extract adjoint state
 #undef ALLOW_AUTODIFF_MONITOR
-C
+
 C       >>> DO 2-level checkpointing instead of 3-level
 #undef AUTODIFF_2_LEVEL_CHECKPOINT
-C
+
 C o use divided adjoint to split adjoint computations
 #undef ALLOW_DIVIDED_ADJOINT
 
 C ********************************************************************
 C ***                Cost function Package                         ***
 C ********************************************************************
-C
+
 C       >>> Cost function contributions
 #define ALLOW_COST_TEST
 
 C ********************************************************************
 C ***               Control vector Package                         ***
 C ********************************************************************
-C
+
 #undef EXCLUDE_CTRL_PACK
 #undef  ALLOW_NONDIMENSIONAL_CONTROL_IO
-C
+
 C       >>> Initial values.
 #define ALLOW_GENARR2D_CONTROL
 
 #endif /* ECCO_CPPOPTIONS_H */
-
