7c7f6d4e4255aa5af377032d86013728d200e160
Gael Forget <gforget@mit.edu>
2014-09-14 21:50:54 +0000

    o pkg/ctrl/CTRL_GENARR.h, ctrl_readparms.F :   - add xx_genarr2d_bounds,xx_genarr3d_bounds,     xx_genarr2d_numsmooth, xx_genarr3d_numsmooth o pkg/ctrl/ctrl_map_ini_genarr.F :   - remove compilation failure   - replace hard coded example   - use ctrl_map_ini_gen2D, 3D o pkg/ctrl/ctrl_pack.F, ctrl_unpack.F :   - on/off switch based upon xx_genarr2d_weight,3d

--- pkg/ctrl/CTRL_GENARR.h
+++ pkg/ctrl/CTRL_GENARR.h
@@ -18,8 +18,16 @@ C     ==================================================================
       CHARACTER*(MAX_LEN_FNAM) xx_genarr3d_weight(maxCtrlArr3D)
 
       COMMON /CONTROLFILES_RARR/
-     &     genarr2dPrecond
+     &     genarr2dPrecond,
+     &     xx_genarr2d_bounds,xx_genarr3d_bounds
       _RL genarr2dPrecond(maxCtrlArr2D)
+      _RL xx_genarr2d_bounds(5,maxCtrlArr2D)
+      _RL xx_genarr3d_bounds(5,maxCtrlArr3D)
+
+      COMMON /CONTROLFILES_IARR/
+     &     xx_genarr2d_numsmooth,xx_genarr3d_numsmooth
+      INTEGER xx_genarr2d_numsmooth(maxCtrlArr2D)
+      INTEGER xx_genarr3d_numsmooth(maxCtrlArr3D)
 
       COMMON /CONTROLFILES_CTIM/
      &     xx_gentim2d_file,
