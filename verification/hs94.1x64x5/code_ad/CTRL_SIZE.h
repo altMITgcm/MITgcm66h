65f84218581834f3ce6d2052ce48f59228a50f7a
Gael Forget <gforget@mit.edu>
2014-11-13 17:05:25 +0000

    - add maxCtrlProc.

--- verification/hs94.1x64x5/code_ad/CTRL_SIZE.h
+++ verification/hs94.1x64x5/code_ad/CTRL_SIZE.h
@@ -10,6 +10,8 @@ C     ----------------------------------------
 C
 C     maxCtrlArr2D :: number of 2-dim. generic control variables
 C     maxCtrlArr3D :: number of 3-dim. generic control variables
+C     maxCtrlTim2D :: number of 2-d generic tim-varying ctrl variables
+C     maxCtrlProc  :: number of pre-processing options per ctrl variable
 
       integer     maxCtrlArr2D
       parameter ( maxCtrlArr2D = 3 )
@@ -19,6 +21,9 @@ C     maxCtrlArr3D :: number of 3-dim. generic control variables
 
       integer     maxCtrlTim2D
       parameter ( maxCtrlTim2D = 1 )
+
+      integer     maxCtrlProc
+      parameter ( maxCtrlProc = 1 )
 
 CEH3 ;;; Local Variables: ***
 CEH3 ;;; mode:fortran ***
