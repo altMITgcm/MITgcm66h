f9bf9609a37f849d9a7ee0a46a288aaffda11098
Gael Forget <gforget@mit.edu>
2014-11-13 14:50:58 +0000

    - add preprocessing run time params - add genarr3dPrecond

--- pkg/ctrl/CTRL_SIZE.h
+++ pkg/ctrl/CTRL_SIZE.h
@@ -13,6 +13,7 @@ C
 C     maxCtrlArr2D :: number of 2-d generic init. ctrl variables
 C     maxCtrlArr3D :: number of 3-d generic init. ctrl variables
 C     maxCtrlTim2D :: number of 2-d generic tim-varying ctrl variables
+C     maxCtrlProc  :: number of pre-processing options per ctrl variable
 
       integer     maxCtrlArr2D
       parameter ( maxCtrlArr2D = 1 )
@@ -22,6 +23,9 @@ C     maxCtrlTim2D :: number of 2-d generic tim-varying ctrl variables
 
       integer     maxCtrlTim2D
       parameter ( maxCtrlTim2D = 1 )
+
+      integer     maxCtrlProc
+      parameter ( maxCtrlProc = 1 )
 
 #endif
 
