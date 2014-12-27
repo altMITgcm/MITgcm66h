f08ffe87cd1d7c49781f4eb0eea88c44088f14a2
Patrick Heimbach <heimbach@mit.edu>
2014-12-27 20:37:57 +0000

    Temporarily remove development code for OpenAD active file

---
+++ pkg/openad/OPENAD_OPTIONS.h
@@ -0,0 +1,26 @@
+C $Header$
+C $Name$
+
+CBOP
+C !ROUTINE: OPENAD_OPTIONS.h
+C !INTERFACE:
+C #include "OPENAD_OPTIONS.h"
+
+C !DESCRIPTION:
+C *==================================================================*
+C | CPP options file for OpenAD (openad) package:
+C | Control which optional features to compile in this package code.
+C *==================================================================*
+CEOP
+
+#ifndef OPENAD_OPTIONS_H
+#define OPENAD_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+
+#ifdef ALLOW_OPENAD
+
+#undef ALLOW_OPENAD_ACTIVE_FILE
+
+#endif /* ALLOW_OPENAD */
+#endif /* OPENAD_OPTIONS_H */
+
