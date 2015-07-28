4e82a3e9af58d3487d0417fb3c3b8026c3093743
Gael Forget <gforget@mit.edu>
2015-07-28 01:00:39 +0000

    - add prof_facmod run time parameter.

--- pkg/profiles/profiles.h
+++ pkg/profiles/profiles.h
@@ -97,10 +97,12 @@ C===========================================================
       COMMON /profiles_cost_r/
      &                objf_profiles,
      &                num_profiles,
-     &                mult_profiles
+     &                mult_profiles,
+     &                prof_facmod
       _RL  objf_profiles(NFILESPROFMAX,NVARMAX,nsx,nsy)
       _RL  num_profiles(NFILESPROFMAX,NVARMAX,nsx,nsy)
       _RL  mult_profiles(NFILESPROFMAX,NVARMAX)
+      _RL  prof_facmod(NFILESPROFMAX,NVARMAX)
 
       COMMON /profiles_cost_c/
      &        profilesDir, profilesfiles
