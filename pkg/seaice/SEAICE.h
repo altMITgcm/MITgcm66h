6f011fd38ee17fcbdc29429b1783172fcbc5849f
Martin Losch <Martin.Losch@awi.de>
2015-09-10 15:47:46 +0000

    add new global field "tensileStrength"

--- pkg/seaice/SEAICE.h
+++ pkg/seaice/SEAICE.h
@@ -120,10 +120,13 @@ C--   Dynamical variables
       _RL vIceC      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 #else
       COMMON/SEAICE_DYNVARS_CGRID/
-     &     seaiceMassC, seaiceMassU, seaiceMassV
-      _RL seaiceMassC(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL seaiceMassU(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL seaiceMassV(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+     &     seaiceMassC, seaiceMassU, seaiceMassV, tensileStrength
+      _RL seaiceMassC    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL seaiceMassU    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL seaiceMassV    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+C     the maximal tensile stress that ice able to withstand, 
+C     in analogy to the maximal compressive stress PRESS0
+      _RL tensileStrength(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 #endif
 
       COMMON/SEAICE_DYNVARS_4/
