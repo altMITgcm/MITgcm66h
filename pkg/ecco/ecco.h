6be4e2d0021dcd5fe455e508cb6be4246a6ebf8e
An T Nguyen <antnguyen13@gmail.com>
2015-11-11 21:43:45 +0000

    change msktrVol[W,S] from 3D to 2D

--- pkg/ecco/ecco.h
+++ pkg/ecco/ecco.h
@@ -147,8 +147,8 @@ c                 the current model integration.
       _RL m_UE (1-olx:snx+olx,1-oly:sny+oly,nr,nsx,nsy)
       _RL m_VN (1-olx:snx+olx,1-oly:sny+oly,nr,nsx,nsy)
 #ifdef ALLOW_GENCOST_TRANSPORT
-      _RL msktrVolW (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      _RL msktrVolS (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL msktrVolW (1-OLx:sNx+OLx,1-OLy:sNy+OLy,   nSx,nSy)
+      _RL msktrVolS (1-OLx:sNx+OLx,1-OLy:sNy+OLy,   nSx,nSy)
       _RL trVol(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL trHeat(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL trSalt(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
