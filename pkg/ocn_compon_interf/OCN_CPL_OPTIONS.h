72128f8c123300fa3b1a231eddde19225781b9b8
Jean-Michel Campin <jmc@ocean.mit.edu>
2015-11-12 00:53:46 +0000

    add OCN_CPL_OPTIONS.h in component pkg and include it in all pkg source files.

---
+++ pkg/ocn_compon_interf/OCN_CPL_OPTIONS.h
@@ -0,0 +1,13 @@
+C $Header$
+C $Name$
+
+#ifndef OCN_CPL_OPTIONS_H
+#define OCN_CPL_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_OCN_COMPON_INTERF
+C     Package-specific Options & Macros go here
+
+#endif /* ALLOW_OCN_COMPON_INTERF */
+#endif /* OCN_CPL_OPTIONS_H */
